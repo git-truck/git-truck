946bd50009abdac562b494593bc9a61fc93e434b
joglr <1959615+joglr@users.noreply.github.com>
2022-02-09 12:05:25 +0100

    Add mock tree data
    Co-authored-by: Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>

---
+++ prototype1/src/data.ts
@@ -0,0 +1,134 @@
+import { GitTreeObject } from "./model";
+
+export const data: GitTreeObject = {
+  name: "root",
+  hash: "e455ef525914d40cb432200dd8d69f5df18ace96",
+  children: [
+    {
+      content: "",
+      hash: "6d3a37b3031f9c2932778690c568802815925d43",
+      name: ".env",
+    },
+    {
+      content: "",
+      hash: "bffb357a7122523ec94045523758c4b825b448ef",
+      name: ".eslintrc.json",
+    },
+    {
+      children: [],
+      hash: "af92ab8c3cad3861a92416ef5f16ebb51c24f6a5",
+      name: ".github",
+    },
+    {
+      content: "",
+      hash: "cdf0e5867523f31ddb744d763f6b64817e821097",
+      name: ".gitignore",
+    },
+    {
+      children: [],
+      hash: "f8c977ef2f1c8205e2cac5efa938f7e01c7a2067",
+      name: ".husky",
+    },
+    {
+      content: "",
+      hash: "bf743a1c1fa4af739b35d5851c7555c796a5321c",
+      name: ".prettierignore",
+    },
+    {
+      children: [],
+      hash: "f08a1bf26a00e2846668b5e8ae8b19ad99af331e",
+      name: ".vscode",
+    },
+    {
+      content: "",
+      hash: "471276e896867af8b8be2cfd5a69645355d543fd",
+      name: "LICENSE",
+    },
+    {
+      content: "",
+      hash: "2cf14ad3e2255a70acd9263e67a7b52ec1b9c25e",
+      name: "README.md",
+    },
+    {
+      content: "",
+      hash: "d8f780cee63b3531483d7c316f9a1b7fae083ce9",
+      name: "TODO.md",
+    },
+    {
+      content: "",
+      hash: "d0de870dc5584ebd2c255281b45dec58788ab59e",
+      name: "jest.setup.ts",
+    },
+    {
+      content: "",
+      hash: "4f11a03dc6cc37f2b5105c08f2e7b24c603ab2f4",
+      name: "next-env.d.ts",
+    },
+    {
+      content: "",
+      hash: "465d9981043573710db9c1f11b56318d5275af00",
+      name: "next.config.js",
+    },
+    {
+      content: "",
+      hash: "8d60a723df383c645e2550cef43240d5802d946b",
+      name: "package.json",
+    },
+    {
+      children: [],
+      hash: "a6b8af1aaebc89b55e04be16195f4e8b89b44602",
+      name: "public",
+    },
+    {
+      children: [
+        {
+          hash: "591a575ff128db0f0c12bee80af412d7d8dd7526",
+          name: "pages",
+          children: [],
+        },
+        {
+          hash: "57f6584fdd2c3b8a3c221267c4b51e4f718cf257",
+          name: "state.test.ts",
+          content: "",
+        },
+        {
+          hash: "573b0621402b12311f34af9ab0d74bfe5d993b33",
+          name: "state.ts",
+          content: "",
+        },
+        {
+          hash: "43623638bc0126bc64f6b63b5fd1e23defba733f",
+          name: "styles",
+          children: [],
+        },
+        {
+          hash: "a8219567eaa4817b6cc3d6c72e9670e3d9cc50d2",
+          name: "util.ts",
+          content: "",
+        },
+        {
+          hash: "fb9e08ce583fa9d465c737e0e900848432a6707e",
+          name: "word.ts",
+          content: "",
+        },
+      ],
+      hash: "3cf53ac8ab34ef3f7d442ad27b3699d56cda8e1f",
+      name: "src",
+    },
+    {
+      content: "",
+      hash: "d4e1de838e800113cbb1a6aa94c12b009f5acd95",
+      name: "tsconfig.json",
+    },
+    {
+      content: "",
+      hash: "cae1acc867089089b180efd0a451c20e06a57088",
+      name: "words.json",
+    },
+    {
+      content: "",
+      hash: "52e99a4e738e15a166cedd0eb5fae60332aaa35d",
+      name: "yarn.lock",
+    },
+  ],
+};
