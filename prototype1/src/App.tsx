233e46159248ac6f9a6d11f9b375af2e48396580
joglr <1959615+joglr@users.noreply.github.com>
2022-02-09 16:36:24 +0100

    Clean up code

--- prototype1/src/App.tsx
+++ prototype1/src/App.tsx
@@ -12,22 +12,31 @@ function useMount(fn: Function) {
 }
 
 const padding = 30
-function App() {
-  let svgRef = useRef<SVGElement>(null)
-  let rootRef = useRef<SVGGElement>(null)
-  let sizeProps = useWindowSize()
+const textSpacingFromCircle = 5
 
-  useMount(() => {
+function App() {
+  let svgRef = useRef<SVGSVGElement>(null)
+  let rootRef =
+    useRef<d3.Selection<SVGGElement, unknown, null, undefined>>(null)
+  let sizeProps = useWindowSize(0, 0)
+  let paddedSizeProps = {
+    height: sizeProps.height - padding * 2,
+    width: sizeProps.width - padding * 2,
+  }
+
+  useEffect(() => {
     let svg = d3.select(svgRef.current)
 
     let root = d3
       .hierarchy(data)
+      // TODO: Derrive size from file/folder size
       .sum(() => 10 + Math.random() * 20)
+      // .sum(() => 10)
       .sort((a, b) => b.value - a.value)
 
     let partition = d3
       .pack()
-      .size([sizeProps.width, sizeProps.height])
+      .size([paddedSizeProps.width, paddedSizeProps.height])
       .padding(padding)
 
     partition(root)
@@ -40,8 +49,9 @@ function App() {
       .enter()
       .append("g")
 
-    group
-      .append("circle")
+    const circle = group.append("circle")
+
+    circle
       // .attr("d", d => circlePathFromCircle(d.x, d.y, d.r))
       .classed("node", true)
       .attr("cx", (d) => d.x)
@@ -50,61 +60,65 @@ function App() {
       .style("fill", (d) => (d.data.children ? "none" : "cadetblue"))
       .style("opacity", 0.3)
       .style("stroke", "black")
-      // .attr("id", (d) => d.data.hash)
+    // .attr("id", (d) => d.data.hash)
 
-    group
-      .append("path")
-      .attr("d", d => circlePathFromCircle(d.x, d.y, d.r + padding / 2))
+    const path = group.append("path")
+
+    path
+      .attr("d", (d) => circlePathFromCircle(d.x, d.y, d.r + padding / 10))
       .classed("node", true)
       .attr("cx", (d) => d.x)
       .attr("cy", (d) => d.y)
       .attr("r", (d) => d.r)
       .style("fill", "transparent")
       .style("opacity", 0.3)
+
       // .style("stroke", "black")
       .attr("id", (d) => d.data.hash)
+    // .attr("marker-end", "url(#arrowhead)")
 
-    group
-      .append("text")
-      // .attr("dx", (d) => "5%")
-      // .attr("direction", "rtl")
-      // .attr("y", (d) => d.y)
-      // .attr("r", (d) => d.r)
-      .attr("dominant-baseline", "middle")
-      .attr("text-achor", "middle")
+    const text = group.append("text")
+
+    text
       .append("textPath")
-      // .attr("textLength", (d) => d.r * (3/4) * Math.PI)
+      .attr("startOffset", "25%")
+      .attr("dominant-baseline", "bottom")
+      .attr("text-anchor", "middle")
+
+      // .attr("textLength", (d) => d.data.name.length * 7)
       .attr("xlink:href", (d) => `#${d.data.hash}`)
       .text((d) => d.data.name)
 
     return () => {
       rootRef.current.remove()
     }
-  })
+  }, [paddedSizeProps])
 
   return (
-    <svg
-      {...sizeProps}
-      ref={svgRef}
+    <div
       style={{
-        transform: "rotate(-55deg)"
+        padding,
       }}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox={`0 0 ${sizeProps.width} ${sizeProps.height}`}
-    ></svg>
+    >
+      <svg
+        {...paddedSizeProps}
+        ref={svgRef}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox={`0 0 ${paddedSizeProps.width} ${paddedSizeProps.height}`}
+      />
+    </div>
   )
 }
+// a rx ry angle large-arc-flag sweep-flag dx dy
+// rx and ry are the two radii of the ellipse
+// angle represents a rotation (in degrees) of the ellipse relative to the x-axis;
+// large-arc-flag and sweep-flag allows to chose which arc must be drawn as 4 possible arcs can be drawn out of the other parameters.
 
 function circlePathFromCircle(x, y, r) {
-  return `M${x},${y}m${-r},0a${r},${r} 0 1,0 ${r * 2},0a${r},${r} 0 1,0 ${
-    -r * 2
-  },0`
-}
-
-function inverseCirclePathFromCricle(x, y, r) {
-  return `M${x},${y}m${r},0a${r},${r} 0 1,0 ${-r * 2},0a${r},${r} 0 1,0 ${
-    r * 2
-  },0`
+  return `M${x},${y}
+          m${-r},0
+          a${r},${r} 0 1,1 ${r * 2},0
+          a${r},${r} 0 1,1 ${-r * 2},0`
 }
 
 export default App
