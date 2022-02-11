d6ee4058a51632f47889457d5e88bc05e85629b0
joglr <1959615+joglr@users.noreply.github.com>
2022-02-11 13:40:52 +0100

    WIP
    Co-authored-by: tjomson <tjomson@users.noreply.github.com>

--- parser/src/index.ts
+++ parser/src/index.ts
@@ -1,3 +1,13 @@
-import { runAsCli } from "./parser.js";
+import { findBranchHead } from "./parse-tree.js"
 
-await runAsCli();
+
+if (process.argv.length < 3) {
+  console.log("Usage: <path to git repository> [branch=main]")
+  process.exit(0)
+}
+
+const dir = process.argv[2]
+const branch = process.argv.length < 4 ? "main" : process.argv[3]
+const result = await findBranchHead(dir, branch)
+console.log(result);
+
