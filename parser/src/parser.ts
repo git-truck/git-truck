405ad0310b9d4aa6e7915683ec021a0ab1f5e57d
joglr <1959615+joglr@users.noreply.github.com>
2022-02-11 12:29:59 +0100

    Rewrite git object parser

--- parser/src/parser.ts
+++ parser/src/parser.ts
@@ -1,33 +1,34 @@
 import { unzip } from 'zlib';
-import { readFileSync, readdirSync } from 'fs'
+import { promises as fs } from 'fs'
 import { promisify } from 'util';
 import  { createInterface } from "readline";
+import path from "path"
 
-export async function parseFile(path: string) {
-    const buffer = readFileSync(path);
-    const unzipPromise = promisify(unzip);
-    const res = await unzipPromise(buffer);
-    return res.toString();
+async function parseFile(path: string) {
+  const buffer = await fs.readFile(path)
+  const unzipPromise = promisify(unzip)
+  const res = await unzipPromise(buffer)
+  return res.toString()
 }
 
 export async function parseGitObjects(directory: string) {
-    const gitObjects = new Map<string, string>();
-    const gitObjectsPath = directory + '/.git/objects'
+  const gitObjectsPath = directory + "/.git/objects"
 
-    let entries = readdirSync(gitObjectsPath);
-    entries = entries.filter(entry => entry !== "pack" && entry !== "info");
+  let folderPaths = await fs.readdir(gitObjectsPath)
+  const gitObjects = new Map<string, string>()
 
-    entries.forEach(entry => {
-        const dir = gitObjectsPath + "/" + entry
-        const fileNames = readdirSync(dir);
-        Promise.all(fileNames.map(async fileName => {
-            const fileContent = await parseFile(dir + "/" + fileName);
-            console.log(fileContent);
-            gitObjects.set(entry + fileName, fileContent);
-        }));
+  for (let folder of folderPaths) {
+    const dir = gitObjectsPath + "/" + folder
+    const fileNames = await fs.readdir(dir)
+    for (let fileName of fileNames) {
+      const hash = folder + fileName
+      const rawObject = await parseFile(path.join(dir, fileName))
 
-    });
-    console.log(gitObjects);
+
+      gitObjects.set(hash, rawObject)
+    }
+  }
+  return gitObjects
 }
 
 export async function runAsCli() {
@@ -36,8 +37,9 @@ export async function runAsCli() {
       output: process.stdout
     });
 
-    io.question("Input path to directory: ", (input) => {
-        parseGitObjects(input);
+    io.question("Input path to directory: ", async (input) => {
+        const result = await parseGitObjects(input);
+        console.log(Array.from(result.entries()))
         io.close();
     });
 }
