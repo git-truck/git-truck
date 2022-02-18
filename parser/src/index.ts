541fedea7fe2c96b206483fa46ce55aace5779f5
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-02-18 14:03:26 +0100

    Detect current branch Fixes #41

--- parser/src/index.ts
+++ parser/src/index.ts
@@ -2,29 +2,25 @@ import "dotenv/config"
 import { hydrateTreeWithAuthorship } from "./hydrate.js"
 import { log } from "./log.js"
 import { findBranchHead, parseCommit } from "./parse.js"
-import { writeRepoToFile } from "./util.js"
+import { getCurrentBranch, getRepoName, writeRepoToFile } from "./util.js"
 
 if (process.argv.length < 3) {
   console.log(
-    "Usage: [path to git repository = .] [branch = main] [outDir = .]"
+    "Usage: [path to git repository = .] [outDir = .] [outFileName = {repo}_{branch}.json] [branch = main]"
   )
   process.exit(0)
 }
 
 const repoDir = process.argv.length < 3 ? "." : process.argv[2]
-const branch = process.argv.length < 4 ? getCurrentBranch() : process.argv[3]
-const outDir = process.argv.length < 5 ? "." : process.argv[4]
+const outDir = process.argv.length < 4 ? "." : process.argv[4]
+const branch = process.argv.length < 6 ? await getCurrentBranch(repoDir) : process.argv[3]
+const outFileName = process.argv.length < 5 ? `${getRepoName(repoDir)}_${branch}.json` : process.argv[5]
 
 try {
   const branchHead = await findBranchHead(repoDir, branch)
   let repoTree = await parseCommit(repoDir, branchHead)
   repoTree = await hydrateTreeWithAuthorship(repoDir, repoTree)
-  await writeRepoToFile(repoTree, repoDir, branch, outDir)
+  await writeRepoToFile(repoTree, outDir, outFileName)
 } catch (e) {
   log.error(e)
-}
-
-function getCurrentBranch() {
-  // TODO: Determine current branch
-  return "main"
 }
