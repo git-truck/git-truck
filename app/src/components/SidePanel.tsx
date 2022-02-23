6b6d387810c39efbcbc661ed41118b69abc395dc
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-02-23 12:47:40 +0100

    Adjust SidePanel styles

--- app/src/components/SidePanel.tsx
+++ app/src/components/SidePanel.tsx
@@ -2,10 +2,13 @@ import styled from "styled-components"
 import { Details } from "./Details"
 import { Options } from "./Options"
 import { Spacer } from "./Spacer"
-import { Box } from "./util"
 
-const SidePanelRoot = styled(Box)`
-  border-radius: 0;
+const SidePanelRoot = styled.aside`
+  background-color: #fff;
+  box-shadow: 0.9px 0.9px 2.7px rgba(0, 0, 0, 0.07),
+    2.2px 2.2px 6.9px rgba(0, 0, 0, 0.048),
+    4.4px 4.4px 14.2px rgba(0, 0, 0, 0.039),
+    9.1px 9.1px 29.2px rgba(0, 0, 0, 0.031), 25px 25px 80px rgba(0, 0, 0, 0.022);
 `
 
 export function SidePanel() {
