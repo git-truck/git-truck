98358ba23ce9ea2726b9dda9b0c71d34c074db86
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2022-02-23 15:48:19 +0100

    Remove Sidepanel background

--- app/src/components/SidePanel.tsx
+++ app/src/components/SidePanel.tsx
@@ -5,11 +5,6 @@ import { Options } from "./Options"
 import { Spacer } from "./Spacer"
 
 const SidePanelRoot = styled.aside`
-  background-color: #fff;
-  box-shadow: 0.9px 0.9px 2.7px rgba(0, 0, 0, 0.07),
-    2.2px 2.2px 6.9px rgba(0, 0, 0, 0.048),
-    4.4px 4.4px 14.2px rgba(0, 0, 0, 0.039),
-    9.1px 9.1px 29.2px rgba(0, 0, 0, 0.031), 25px 25px 80px rgba(0, 0, 0, 0.022);
 `
 
 export function SidePanel() {
