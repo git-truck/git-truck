19b4480698f2a1fb8385d4200368801d02974fb5
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-02-23 10:16:37 +0100

    Rename clsx to classes

--- app/src/components/Box.tsx
+++ app/src/components/Box.tsx
@@ -1,13 +1,13 @@
 import "./Box.css"
 import { HTMLProps, PropsWithChildren } from "react"
-import { clsx } from "../util"
+import { classes } from "../util"
 import { Spacer } from "./Spacer"
 
 type BoxTitleProps = PropsWithChildren<HTMLProps<HTMLDivElement>>
 
 export function BoxTitle({ children, className, ...props }: BoxTitleProps) {
   return (
-    <h2 {...props} className={clsx("box-title", className)}>
+    <h2 {...props} className={classes("box-title", className)}>
       {children}
     </h2>
   )
@@ -19,7 +19,7 @@ type BoxProps = PropsWithChildren<HTMLProps<HTMLDivElement>> & {
 
 export function Box({ title, children, className, ...props }: BoxProps) {
   return (
-    <div {...props} className={clsx("box", className)}>
+    <div {...props} className={classes("box", className)}>
       {title ? (
         <>
           <BoxTitle>{title}</BoxTitle>
