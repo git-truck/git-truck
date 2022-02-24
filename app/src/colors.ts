4c8b3307970a8572d762b0c2daa903b6a7c11b64
Thomas Hoffmann Kilbak <thki@queue-it.com>
2022-02-24 10:33:26 +0100

    Remove unused vars

--- app/src/colors.ts
+++ app/src/colors.ts
@@ -4,7 +4,6 @@ import { MutableRefObject } from "react"
 import { HydratedGitBlobObject } from "../../parser/src/model"
 import { authorColorState } from "./components/BubbleChart"
 import { unionAuthors } from "./util"
-import { users } from "./const"
 
 export function getExtensionColor(
   legendSetRef: MutableRefObject<Set<string>>,
