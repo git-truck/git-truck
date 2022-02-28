a036b599b1c6447b51ce252fdc6058afbe8f44ba
Thomas Hoffmann Kilbak <thki@queue-it.com>
2022-02-28 12:34:02 +0100

    Fix import/export

--- parser/src/TruckIgnore.ts
+++ parser/src/TruckIgnore.ts
@@ -1,21 +1,20 @@
-import { readFileSync } from 'fs'
-import { compile } from 'gitignore-parser'
-import { warn } from './log.js'
+import { readFileSync } from "fs"
+import { compile } from "gitignore-parser"
+import { warn } from "./log"
 
 export default class TruckIgnore {
-    private truckignore
-    constructor(path: string) {
-        try {
-            const file = readFileSync(path + "/.truckignore", 'utf-8')
-            this.truckignore = compile(file)
-        } catch(e) {
-            warn("No .truckignore found")
-        }
+  private truckignore
+  constructor(path: string) {
+    try {
+      const file = readFileSync(path + "/.truckignore", "utf-8")
+      this.truckignore = compile(file)
+    } catch (e) {
+      warn("No .truckignore found")
     }
+  }
 
-    public isAccepted(fileName: string) {
-        if (!this.truckignore) return true
-        return this.truckignore.accepts(fileName)
-    }
+  public isAccepted(fileName: string) {
+    if (!this.truckignore) return true
+    return this.truckignore.accepts(fileName)
+  }
 }
-
