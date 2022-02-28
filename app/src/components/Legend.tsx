2c12644e4c6f48df333ff29687fb897d656e0fe8
emiljapelt <55390848+emiljapelt@users.noreply.github.com>
2022-02-28 13:06:52 +0100

    Refactor Legend cutoff

--- app/src/components/Legend.tsx
+++ app/src/components/Legend.tsx
@@ -5,25 +5,30 @@ import { useState } from "react"
 import { useStore } from "../StoreContext"
 import { LegendBox } from "./util"
 
+const cutoff = 5
+
 export function Legend() {
   const { metricType, metricCaches } = useStore()
   let items = Array.from(metricCaches.get(metricType)?.legend ?? [])
 
   const [collapse, setCollapse] = useState<boolean>(true)
   if (items.length === 0) return null
-  return (
-    <LegendBox>
-      <LegendFragment show={true} items={items.slice(0, 3)} />
-      {items.length > 3 ? (
-        <>
-          <LegendFragment show={!collapse} items={items.slice(3)} />
-          <LegendOther show={collapse} items={items.slice(3)} />
-          <LegendToggle
-            collapse={collapse}
-            toggle={() => setCollapse(!collapse)}
-          />
-        </>
-      ) : null}
-    </LegendBox>
-  )
+  if (items.length <= cutoff + 1)
+    return (
+      <LegendBox>
+        <LegendFragment show={true} items={items}></LegendFragment>
+      </LegendBox>
+    )
+  else
+    return (
+      <LegendBox>
+        <LegendFragment show={true} items={items.slice(0, cutoff)} />
+        <LegendFragment show={!collapse} items={items.slice(cutoff)} />
+        <LegendOther show={collapse} items={items.slice(cutoff)} />
+        <LegendToggle
+          collapse={collapse}
+          toggle={() => setCollapse(!collapse)}
+        />
+      </LegendBox>
+    )
 }
