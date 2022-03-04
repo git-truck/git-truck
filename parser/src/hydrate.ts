1f8caf9b2845be94c4510549ed680b605caf2577
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2022-03-04 11:11:14 +0100

    Handle merge commits (#231)
    * Fix when description is undefined
    
    * Add bfs traversal to the hydrate function
    
    Misc:
    - Rename emptyGitTree to emptyGitCommitHash
    - Rename hydrateTreeWithAuthorship to hydrateData
    
    * Fix eslint, prefer const
    
    * Fix bfs loop
    
    * Add credit to queue

--- parser/src/hydrate.ts
+++ parser/src/hydrate.ts
@@ -1,119 +1,173 @@
+import { emptyGitCommitHash } from "./constants"
 import isBinaryPath from "is-binary-path"
 import { join } from "path"
-import { emptyGitTree } from "./constants"
 import { log } from "./log"
 import {
   GitBlobObject,
   GitCommitObject,
+  GitCommitObjectLight,
   HydratedGitBlobObject,
   HydratedGitCommitObject,
   HydratedGitTreeObject,
+  PersonWithTime,
 } from "./model"
 import { parseCommitLight } from "./parse"
 import { gitDiffNumStatParsed, lookupFileInTree } from "./util"
+import { Queue } from "./queue"
 
-export async function hydrateTreeWithAuthorship(
+export async function hydrateData(
   repo: string,
   commit: GitCommitObject
 ): Promise<HydratedGitCommitObject> {
-  const hydratedCommit = {
-    ...commit,
-    minNoCommits: Number.MAX_VALUE,
-    maxNoCommits: Number.MIN_VALUE,
-  } as HydratedGitCommitObject
-  let { hash: child, parent } = hydratedCommit
 
-  addAuthorsField(hydratedCommit.tree)
+  const data = commit as HydratedGitCommitObject
 
-  let isDone = false
-  while (!isDone) {
-    if (parent === emptyGitTree) {
-      // We have reached the root of the tree, so we need to compare the initial commit to the empty git tree
-      isDone = true
+  initially_mut(data)
+
+  const { hash: first } = data
+
+  await bfs(first, repo, data)
+
+  finally_mut(data)
+
+  return data
+}
+
+function initially_mut(data: HydratedGitCommitObject) {
+  data.minNoCommits = Number.MAX_VALUE
+  data.maxNoCommits = Number.MIN_VALUE
+  
+  addAuthorsField_mut(data.tree)
+}
+
+function addAuthorsField_mut(tree: HydratedGitTreeObject) {
+  for (const child of tree.children) {
+    if (child.type === "blob") {
+      child.authors = {}
+    } else {
+      addAuthorsField_mut(child)
     }
+  }
+}
 
-    const childCommit = await parseCommitLight(repo, child)
-    if (childCommit.parent2 !== null) {
-      // TODO: Handle merges
+async function bfs(first: string, repo: string, data: HydratedGitCommitObject) {
+  const expandedHashes = new Set<string>()
+  const queue = new Queue<string>()
+
+  queue.enqueue(first)
+
+  while (!queue.isEmpty()) {
+    const currHash = queue.dequeue()
+
+    if (expandedHashes.has(currHash)) continue
+
+    expandedHashes.add(currHash)
+
+    // don't compare the empty commit to it's parent
+    if (currHash == emptyGitCommitHash) continue
+
+    const currCommit = await parseCommitLight(repo, currHash)
+
+    const parentsOfCurr = parents(currCommit)
+
+    for (const parentHash of parentsOfCurr) {
+      switch (parentsOfCurr.size) {
+        case 2: // curr is a merge commit
+          queue.enqueue(parentHash)
+          break;
+        case 1: // curr is a linear commit
+          diffAndUpdate_mut(data, currCommit, parentHash, repo)
+          queue.enqueue(parentHash)
+          break;
+        default: // curr is the root commit
+          diffAndUpdate_mut(data, currCommit, emptyGitCommitHash, repo)
+          break;
+      }
     }
-    const { author, ...restof } = childCommit
-    log.debug(
-      `[${restof.message.split("\n").filter((x) => x.trim().length > 0)[0]}]`
-    )
-    // Diff newer with current
-    log.debug(`comparing [${child}] -> [${parent}]`)
+  }
+}
 
-    const results = await gitDiffNumStatParsed(repo, child, parent)
-    let numTimesCredited = 0
+function parents(obj : GitCommitObject|GitCommitObjectLight) : Set<string> {
+  const parents = new Set<string>()
 
-    for (const { pos, neg, file } of results) {
-      const blob = await lookupFileInTree(hydratedCommit.tree, file)
+  if (obj.parent !== null)
+    parents.add(obj.parent)
+  if (obj.parent2 !== null)
+    parents.add(obj.parent2)
+
+  return parents
+}
+
+async function diffAndUpdate_mut(data: HydratedGitCommitObject, currCommit: GitCommitObjectLight, parentHash: string, repo: string) {
+    const { author } = currCommit
+
+    const currHash = currCommit.hash
+
+    log.debug(`comparing [${currHash}] -> [${parentHash}]`)
+
+    const fileChanges = await gitDiffNumStatParsed(repo, currHash, parentHash)
+
+    for (const fileChange of fileChanges) {
+
+      const { pos, neg, file } = fileChange
+
+      const blob = await lookupFileInTree(data.tree, file)
 
       if (file === "dev/null") continue
       if (blob) {
         if (isBinaryFile(blob)) continue
-        numTimesCredited++
-        const noCommits = 1 + ((blob as HydratedGitBlobObject).noCommits ?? 0)
-        if (noCommits < hydratedCommit.minNoCommits)
-          hydratedCommit.minNoCommits = noCommits
-        if (noCommits > hydratedCommit.maxNoCommits)
-          hydratedCommit.maxNoCommits = noCommits
-        const hydratedBlob = {
-          ...blob,
-          authors: (blob as HydratedGitBlobObject).authors ?? {},
-          noLines: blob.content.split("\n").length,
-          noCommits: noCommits,
-        } as HydratedGitBlobObject
-
-        const current = hydratedBlob.authors?.[author.name] ?? 0
-
-        for (const coauthor of childCommit.coauthors) {
-          hydratedBlob.authors[coauthor.name] = current + pos + neg
-        }
-
-        hydratedBlob.authors[author.name] = current + pos + neg
-        // Log out the authorship
-        log.debug(
-          `${file} ${hydratedBlob.authors[author.name]} (${
-            author.name
-          } +${pos} -${neg})`
-        )
-        Object.assign(blob, hydratedBlob)
+        updateBlob_mut(blob, data, author, currCommit, pos, neg)
       }
     }
-    if (numTimesCredited === 0) {
-      log.debug("Commit has no authorship (file probably does no longer exist)")
-    }
-    if (!isDone) {
-      child = parent
-      parent = (await parseCommitLight(repo, childCommit.parent)).parent
-    }
-  }
-  discardContentField(hydratedCommit.tree)
-
-  return hydratedCommit
 }
 
 function isBinaryFile(blob: GitBlobObject) {
   return isBinaryPath(join(blob.path, blob.name))
 }
 
-function addAuthorsField(tree: HydratedGitTreeObject) {
-  for (const child of tree.children) {
-    if (child.type === "blob") {
-      child.authors = {}
-    } else {
-      addAuthorsField(child)
-    }
+function updateBlob_mut(
+  blob: GitBlobObject,
+  data: HydratedGitCommitObject,
+  author: PersonWithTime,
+  currCommit: GitCommitObjectLight,
+  pos: number,
+  neg: number
+) {
+  const noCommits = 1 + ((blob as HydratedGitBlobObject).noCommits ?? 0)
+
+  if (noCommits < data.minNoCommits)
+    data.minNoCommits = noCommits
+  if (noCommits > data.maxNoCommits)
+    data.maxNoCommits = noCommits
+
+  const hydratedBlob = {
+    ...blob,
+    authors: (blob as HydratedGitBlobObject).authors ?? {},
+    noLines: blob.content.split("\n").length,
+    noCommits: noCommits,
+  } as HydratedGitBlobObject
+
+  const current = hydratedBlob.authors?.[author.name] ?? 0
+
+  for (const coauthor of currCommit.coauthors) {
+    hydratedBlob.authors[coauthor.name] = current + pos + neg
   }
+
+  hydratedBlob.authors[author.name] = current + pos + neg
+
+  Object.assign(blob, hydratedBlob)
 }
 
-function discardContentField(tree: HydratedGitTreeObject) {
+function finally_mut(data : HydratedGitCommitObject) {
+  discardContentField_mut(data.tree)
+}
+
+function discardContentField_mut(tree: HydratedGitTreeObject) {
   for (const child of tree.children) {
     if (child.type === "blob") {
       child.content = ""
     } else {
-      discardContentField(child)
+      discardContentField_mut(child)
     }
   }
 }
