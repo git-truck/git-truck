b02ed38766741400d8d0e0b30010fd86a0347040
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-03-08 18:08:35 +0100

    Use official GitHub colors and add "Other"

---
+++ app/src/extension-color.ts
@@ -0,0 +1,34 @@
+import gitcolors from "github-colors/colors.json"
+import langMap from "lang-map"
+
+interface ColorResult {
+  lang: string
+  color: string | null
+}
+
+const lowercasedColors = new Map<string, ColorResult>()
+for (const [key, value] of Object.entries(gitcolors)) {
+  lowercasedColors.set(key.toLowerCase(), {
+    ...value,
+    lang: key,
+  })
+}
+
+export function getColorFromExtension(extension: string) {
+  const langMatches = langMap.languages(extension.toLowerCase())
+  const langs = []
+  if (!langMatches) return null
+  let colorResult = null
+  // Loop through lang resuts
+  for (const langResult of langMatches) {
+    // If we have a color for the language, return it
+    let match = lowercasedColors.get(langResult)
+    if (match) {
+      colorResult = match
+      langs.push(colorResult.lang)
+      break
+    }
+  }
+  if (!colorResult) return null
+  return colorResult.color
+}
---
+++ app/src/lang-map.d.ts
@@ -0,0 +1,3 @@
+declare module "lang-map" {
+  export function languages(extension: string): string[]
+}
