c86d41aa655a897e82f221b0266c8f1d7b208b22
emiljapelt <55390848+emiljapelt@users.noreply.github.com>
2022-03-14 11:04:20 +0100

    remove unused import in Chart

--- app/src/components/Chart.tsx
+++ app/src/components/Chart.tsx
@@ -1,5 +1,5 @@
 import "./Chart.css"
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import {
   HydratedGitBlobObject,
   HydratedGitCommitObject,
