20828d5c48f3845e15e403f431d2153cfe782dc7
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-03-16 17:25:11 +0100

    Use resolve for static file locations

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import compression from "compression";
 import morgan from "morgan";
 import open from "open";
 import { createRequestHandler } from "@remix-run/express";
+import { resolve } from "path";
 
 import * as serverBuild from "@remix-run/dev/server-build";
 
@@ -16,12 +17,12 @@ app.disable("x-powered-by");
 // Remix fingerprints its assets so we can cache forever.
 app.use(
   "/build",
-  express.static("public/build", { immutable: true, maxAge: "1y" })
+  express.static(resolve(__dirname, "./public/build"), { immutable: true, maxAge: "1y" })
 );
 
 // Everything else (like favicon.ico) is cached for an hour. You may want to be
 // more aggressive with this caching.
-app.use(express.static("public/build", { maxAge: "1h" }));
+app.use(express.static(resolve(__dirname, "./public/build"), { maxAge: "1h" }));
 
 app.use(morgan("tiny"));
 
