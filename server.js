862a546f8baf6611827c9da4dad9f9c7b5698b8d
emiljapelt <55390848+emiljapelt@users.noreply.github.com>
2022-03-21 09:49:32 +0100

    Dont find random port, try the next port instead

--- server.js
+++ server.js
@@ -41,17 +41,22 @@ app.all(
 let port = process.env.PORT || 3000;
 
 function printOpen(server) {
-  const port = server.address().port
+  const serverport = server.address().port
   console.log(`Git Truck v${gitTruckPackage.version}`);
   console.log(`Serving static assets from ${staticAssetsPath}`);
-  console.log(`Express server listening on port ${port}`);
-  open("http://localhost:" + port);
+  if (serverport !== port) console.log("Default/Specified port was used by another process");
+  console.log(`Express server listening on port ${serverport}`);
+  open("http://localhost:" + serverport);
 }
 
-let server = app.listen(port)
-  .on('error', () => {
-    server.close(() => {
-      server = app.listen()
-        .once('listening', () => printOpen(server))});
-  })
-  .once('listening', () => printOpen(server));
+function startServer(port) {
+  let server = app.listen(port)
+    .on('error', () => {
+      server.close(() => {
+        startServer(port+1);
+      })
+    })
+    .once('listening', () => printOpen(server));
+}
+
+startServer(port);
