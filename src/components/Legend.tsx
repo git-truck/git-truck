3e4cd36d7ec9137854a8a18182afb3484ab74be8
emiljapelt <55390848+emiljapelt@users.noreply.github.com>
2022-03-23 21:41:54 +0100

    Implement gradient arrow

--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -7,13 +7,32 @@ import { GradLegendData, isGradientMetric, PointLegendData } from "../metrics"
 import { useMetricCaches } from "../contexts/MetricContext"
 import { useOptions } from "../contexts/OptionsContext"
 import { Box } from "./util"
+import { useClickedBlob } from "~/contexts/ClickedContext"
+import styled from "styled-components"
 
 const legendCutoff = 3
+
+function getLightness(hsl: string) : number {
+  const regex = /%,((?:\d|\.)+?)%\)/gm
+  let ent = regex.exec(hsl)?.entries()
+  ent?.next()
+  const res = parseFloat(ent?.next().value[1] ?? "-1")
+  return res
+}
+
+const GradArrow = styled.i<{ vis: boolean, pos: number }>`
+  display: ${({vis}) => (vis)? "block" : "none"};
+  transition: 500ms;
+  position: relative;
+  bottom: 11px;
+  left: calc(${({pos}) => pos*100}% - 8px);
+`
 
 export function Legend() {
   const { metricType } = useOptions()
   const metricCaches = useMetricCaches()
   const [collapse, setCollapse] = useState<boolean>(true)
+  const {clickedBlob} = useClickedBlob()
 
   if (!isGradientMetric(metricType)) {
     let items = Array.from(
@@ -52,6 +71,14 @@ export function Legend() {
     let [minValue, maxValue, minColor, maxColor] = metricCaches.get(metricType)
       ?.legend as GradLegendData
 
+    const blobLightness = getLightness(metricCaches.get(metricType)?.colormap.get(clickedBlob?.path ?? "") ?? "")
+    let offset = -1
+    if (blobLightness !== -1) {
+      const min = getLightness(minColor)
+      const max = getLightness(maxColor)
+      offset = (blobLightness - min) / (max - min)
+    }
+
     return (
       <Box>
         <GradientLegendDiv>
@@ -59,6 +86,7 @@ export function Legend() {
           <LegendLable>{maxValue}</LegendLable>
         </GradientLegendDiv>
         <LegendGradient min={minColor} max={maxColor} />
+        <GradArrow vis={offset !== -1} pos={offset}>{'\u25B2'}</GradArrow>
       </Box>
     )
   }
---
+++ src/contexts/ClickedContext.ts
@@ -0,0 +1,17 @@
+import { createContext, Dispatch, SetStateAction, useContext } from "react"
+import { HydratedGitBlobObject } from "~/parser/model"
+
+export interface ClickedBlob {
+  clickedBlob: HydratedGitBlobObject | null
+  setClickedBlob: Dispatch<SetStateAction<HydratedGitBlobObject | null>>
+}
+
+export const ClickedBlobContext = createContext<ClickedBlob | null>(null)
+
+export function useClickedBlob() {
+  const context = useContext(ClickedBlobContext)
+  if (!context) {
+    throw new Error("useClickedBlob must be used within a ClickedBlobProvider")
+  }
+  return context
+}
