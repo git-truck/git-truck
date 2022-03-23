aeb0bee8c345cb861793661542db28a88e95fa75
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-03-23 13:09:02 +0100

    Use ref for focus ctrl + f focus

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import { SearchField, Box, Label } from "./util"
 import styled from "styled-components"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useDebounce } from "react-use"
 import { Spacer } from "./Spacer"
 import { useSearch } from "../contexts/SearchContext"
@@ -12,31 +12,32 @@ const StyledBox = styled(Box)`
 `
 
 export default function SearchBar() {
+  const searchFieldRef = useRef<HTMLInputElement>(null)
   const { setSearchText } = useSearch()
   const [value, setValue] = useState("")
   const id = useId()
 
   useDebounce(() => setSearchText(value), 200, [value])
 
-  const searchOverride = (event: KeyboardEvent) => {
-    if (event.ctrlKey && event.key === "f") {
-      event.preventDefault()
-      document.getElementById(id)?.focus()
-    }
-  }
-
   useEffect(() => {
+    const searchOverride = (event: KeyboardEvent) => {
+      if (event.ctrlKey && event.key === "f") {
+        event.preventDefault()
+        searchFieldRef.current?.focus()
+      }
+    }
     document.body.addEventListener("keydown", searchOverride)
     return () => {
       document.body.removeEventListener("keydown", searchOverride)
     }
-  })
+  }, [])
 
   return (
     <StyledBox>
       <Label htmlFor={id}>Search</Label>
       <Spacer xs />
       <SearchField
+        ref={searchFieldRef}
         id={id}
         value={value}
         type="search"
