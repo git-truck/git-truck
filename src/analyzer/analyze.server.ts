482bc79ed1413dac31fa2d81d5cb541c0164867e
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-04-10 17:07:17 +0200

    Update analyze.server.ts

--- src/analyzer/analyze.server.ts
+++ src/analyzer/analyze.server.ts
@@ -235,7 +235,6 @@ export async function analyze(args: TruckConfig) {
     await git.resetGitSetting("diff.renames", renamesDefaultValue)
     await git.resetGitSetting("diff.renameLimit", renameLimitDefaultValue)
 
-    // Change this if this file moves:
     const defaultOutPath = GitCaller.getCachePath(repoName, branchName)
     if (hydratedRepoTreeError) throw hydratedRepoTreeError
 
