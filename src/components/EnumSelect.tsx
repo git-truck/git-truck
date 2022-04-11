4fcce0c55285c1f9529c642a0f890324aa8c16d4
Thomas Hoffmann Kilbak <thki@queue-it.com>
2022-04-11 12:27:31 +0200

    Don't show Authorship data select when it is not relevant #509

--- src/components/EnumSelect.tsx
+++ src/components/EnumSelect.tsx
@@ -2,20 +2,23 @@ import { useId } from "@react-aria/utils"
 import { Spacer } from "./Spacer"
 import { Label, Select } from "./util"
 
-interface EnumSelectProps<T> {
+interface EnumSelectProps<T extends string> {
   label: string
-  enum: T
-  onChange: (metric: keyof T) => void
+  enum: Record<T, string>
+  onChange: (metric: T) => void
+  hidden?: boolean
 }
 
-export function EnumSelect<T>(props: EnumSelectProps<T>) {
+export function EnumSelect<T extends string>(props: EnumSelectProps<T>) {
   const id = useId()
+  const enumEntries = Object.entries(props.enum) as [T, string][]
+
   return (
-    <div>
+    <div style={props.hidden ? { visibility: "hidden" } : {}}>
       <Label htmlFor={id}>{props.label}</Label>
       <Spacer xs />
-      <Select id={id} onChange={(event) => props.onChange(event.target.value as unknown as keyof T)}>
-        {Object.entries(props.enum).map(([key, value]) => (
+      <Select id={id} onChange={(event) => props.onChange(event.target.value as T)}>
+        {enumEntries.map(([key, value]) => (
           <option key={value} value={key}>
             {value}
           </option>
