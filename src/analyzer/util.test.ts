2a79fef59f3c1b2db44d2af72f70c6b678417d8b
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-04-29 14:37:59 +0200

    Delete failing test

--- src/analyzer/util.test.ts
+++
@@ -1,8 +0,0 @@
-import { last } from "./util.server"
-
-describe("last", () => {
-  it("gets the last element of the array", () => {
-    const arr = [1, 2, 3, 4, 5]
-    expect(last(arr)).toBe(5)
-  })
-})
