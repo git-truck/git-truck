1b624484b63722e5660ca52700cf359e8a967741
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-10-05 12:45:08 +0300

    Shorten scripts

--- scripts/publish-experimental.js
+++ scripts/publish-experimental.js
@@ -1,15 +1,43 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { execSync } = require("child_process")
 
+const hasUncommittedChanges = execSync("git status --porcelain", { stdio: "pipe" })?.toString().trim().length > 0
+
 async function main() {
-  const commitHash = execSync("git rev-parse --short HEAD").toString().trim()
+
+  if (hasUncommittedChanges) {
+    console.error("You have uncommitted changes. Please commit or stash them before publishing.")
+    process.exit(1)
+  }
+
+  if (!process.argv[2]) {
+    console.error("You must specify a OTP number to publish.")
+    process.exit(1)
+  }
+
+  const commitHash = execSync("git rev-parse --short HEAD", { stdio: "pipe" }).toString().trim()
   const versionTag = `0.0.0-${commitHash}`
-  execSync(`npm version ${versionTag} --no-git-tag-version`)
-  execSync(`npm publish --tag experimental`)
+
+
+  console.log(`Tagging version ${versionTag}...`)
+  execSync(`npm version ${versionTag} --no-git-tag-version`, { stdio: "pipe" })
+  console.log(`Tagged version ${versionTag}!`)
+  console.log(`Publishing version ${versionTag} to git-truck@experimental...`)
+  execSync(`npm publish --otp ${process.argv[2]} --tag experimental`, { stdio: "inherit" })
+  console.log(`Published version ${versionTag} to git-truck@experimental!`)
+  console.log("Cleaning up...")
+  cleanUp()
+  console.log("Cleaned up!")
 }
 
 main().catch((err) => {
+  cleanUp()
   console.error(err)
   process.exit(1)
 })
 
+process.on("SIGTERM", cleanUp)
+
+function cleanUp() {
+  if (!hasUncommittedChanges) execSync(`git checkout HEAD -- package.json package-lock.json`, { stdio: "pipe" })
+}
