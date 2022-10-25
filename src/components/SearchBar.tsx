160775347a1716a9076995a748ef2f6a906d3f26
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-10-25 10:00:53 +0200

    Use React 18 concurrent features in Search

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,6 @@
 import { SearchField, Box, BoxP, SearchResultButton } from "./util"
 import styled from "styled-components"
-import { Fragment, useEffect, useRef, useState } from "react"
-import { useDebounce } from "react-use"
+import { Fragment, memo, useEffect, useRef, useTransition } from "react"
 import { Spacer } from "./Spacer"
 import { useSearch } from "../contexts/SearchContext"
 import { useId } from "@react-aria/utils"
@@ -36,29 +35,10 @@ function findSearchResults(tree: HydratedGitTreeObject, searchString: string) {
 
 export default function SearchBar() {
   const searchFieldRef = useRef<HTMLInputElement>(null)
-  const { searchText, setSearchText, searchResults, setSearchResults } = useSearch()
-  const [value, setValue] = useState("")
+  const [isTransitioning, startTransition] = useTransition()
+  const { setSearchText, searchText, searchResults, setSearchResults } = useSearch()
   const id = useId()
   const { analyzerData } = useData()
-  const { setPath } = usePath()
-  const { setClickedObject } = useClickedObject()
-
-  function onClick(object: HydratedGitObject) {
-    setClickedObject(object)
-    if (object.type === "tree") {
-      setPath(object.path)
-    } else {
-      const sep = getSeparator(object.path)
-      setPath(allExceptLast(object.path.split(sep)).join(sep))
-    }
-  }
-
-  function debounceUpdate() {
-    setSearchText(value)
-    setSearchResults(findSearchResults(analyzerData.commit.tree, value))
-  }
-
-  useDebounce(() => debounceUpdate(), 200, [value])
 
   useEffect(() => {
     const searchOverride = (event: KeyboardEvent) => {
@@ -78,35 +58,53 @@ export default function SearchBar() {
       <SearchField
         ref={searchFieldRef}
         id={id}
-        value={value}
         type="search"
         placeholder="Search for a file or folder..."
         onChange={(event) => {
-          setValue(event.target.value)
+          const value = event.target.value
+          startTransition(() => {
+            setSearchText(value)
+            setSearchResults(findSearchResults(analyzerData.commit.tree, value))
+          })
         }}
       />
-      {searchText.length > 0 ? (
-        <>
-          <BoxP>{searchResults.length} results</BoxP>
-          {searchResults.length > 0 ? <Spacer /> : null}
-        </>
-      ) : null}
-
-      {searchResults.map((result) => {
-        return (
-          <Fragment key={result.path}>
-            <SearchResultButton title={result.path} value={result.path} onClick={() => onClick(result)}>
-              {result.type === "tree" ? (
-                <FolderIcon display="inline-block" height="1rem" />
-              ) : (
-                <FileIcon display="inline-block" height="1rem" />
-              )}
-              <span>{result.name}</span>
-            </SearchResultButton>
-            <Spacer xs />
-          </Fragment>
-        )
-      })}
+      <BoxP>{isTransitioning ? "Searching..." : searchText.length > 0 ? `${searchResults.length} results` : null}</BoxP>
+      {searchResults.length > 0 ? <Spacer /> : null}
+      <SearchResults />
     </StyledBox>
   )
 }
+
+const SearchResults = memo(function SearchResults() {
+  const { setPath } = usePath()
+  const { setClickedObject } = useClickedObject()
+  const { searchResults } = useSearch()
+
+  function onClick(object: HydratedGitObject) {
+    setClickedObject(object)
+    if (object.type === "tree") {
+      setPath(object.path)
+    } else {
+      const sep = getSeparator(object.path)
+      setPath(allExceptLast(object.path.split(sep)).join(sep))
+    }
+  }
+
+  return (
+    <>
+      {searchResults.map((result) => (
+        <Fragment key={result.path}>
+          <SearchResultButton title={result.path} value={result.path} onClick={() => onClick(result)}>
+            {result.type === "tree" ? (
+              <FolderIcon display="inline-block" height="1rem" />
+            ) : (
+              <FileIcon display="inline-block" height="1rem" />
+            )}
+            <span>{result.name}</span>
+          </SearchResultButton>
+          <Spacer xs />
+        </Fragment>
+      ))}
+    </>
+  )
+})
