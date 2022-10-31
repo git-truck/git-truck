ecbad5d8d2c35393839ad81ab4626efba22ee3e0
Mircea Filip Lungu <mircea.lungu@gmail.com>
2022-11-01 01:42:37 +0200

    changing back to `` because () does not work on my mac actually

--- run-with-docker.sh
+++ run-with-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Note: tested on Mac OS and Linux
 docker build -t gt .
-docker run --rm -it -u (id -u) -v (pwd):(pwd) -w (pwd) -p 3000:3000 gt /usr/app/node_modules/.bin/git-truck --headless 
+docker run --rm -it -u `id -u` -v `pwd`:`pwd` -w `pwd` -p 3000:3000 gt /usr/app/node_modules/.bin/git-truck --headless 
 
 
