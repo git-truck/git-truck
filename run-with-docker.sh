ba868ef929523b9ff122d2d61e5c0147850133cd
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-11-02 18:07:53 +0100

    Test on Windows

--- run-with-docker.sh
+++ run-with-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Note: tested on Mac OS and Linux
+# Note: tested on Mac OS, Linux and Windows MINGW64
 docker build -t gt .
-docker run --rm -it -u `id -u` -v `pwd`:`pwd` -w `pwd` -p 3000:3000 gt /usr/app/node_modules/.bin/git-truck --headless 
+docker run --rm -it -u `id -u` -v `pwd`:`pwd` -w `pwd` -p 3000:3000 gt /usr/app/node_modules/.bin/git-truck --headless
 
 
