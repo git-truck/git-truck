853e31c87cce51f560fae540434830ca704d19ad
Mircea Filip Lungu <mircea.lungu@gmail.com>
2022-11-09 15:04:26 +0200

    Update run-with-docker.sh (#604)
    * Update run-with-docker.sh
    
    * Update run-with-docker.sh
    
    Co-authored-by: Jonas Røssum <1959615+joglr@users.noreply.github.com>

--- run-with-docker.sh
+++ run-with-docker.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-# Note: tested on Mac OS, Linux and Windows MINGW64
-docker build -t gt .
-docker run --rm -it -u `id -u` -v `pwd`:`pwd` -w `pwd` -p 3000:3000 gt /usr/app/node_modules/.bin/git-truck --headless
+
+docker run --rm -it -u `id -u` -v `pwd`:`pwd` -w `pwd` -p 3000:3000  gittruck/git-truck /usr/app/node_modules/.bin/git-truck --headless
+
+# Alternatively, if you want to build locally before running, comment out the above line and uncomment the two lines below:
+# docker build -t gt .
+# docker run --rm -it -u `id -u` -v `pwd`:`pwd` -w `pwd` -p 3000:3000 gt /usr/app/node_modules/.bin/git-truck --headless
+
+# Note: Above tested on Mac OS, Linux and Windows MINGW64
 
 
