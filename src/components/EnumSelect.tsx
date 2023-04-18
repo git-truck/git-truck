4a7062007918b6b25438c14389d77aaaf62a841b
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2023-04-18 14:49:42 +0000

    Migrate Label

--- src/components/EnumSelect.tsx
+++ src/components/EnumSelect.tsx
@@ -1,6 +1,6 @@
 import { useId } from "react"
 import { Spacer } from "./Spacer"
-import { Label, SelectWithEllipsis } from "./util"
+import { SelectWithEllipsis } from "./util"
 
 interface EnumSelectProps<T extends string> {
   label: string
@@ -15,7 +15,9 @@ export function EnumSelect<T extends string>(props: EnumSelectProps<T>) {
 
   return (
     <div style={props.hidden ? { display: "none" } : {}}>
-      <Label htmlFor={id}>{props.label}</Label>
+      <label className="label" htmlFor={id}>
+        {props.label}
+      </label>
       <Spacer xs />
       <SelectWithEllipsis id={id} onChange={(event) => props.onChange(event.target.value as T)}>
         {enumEntries.map(([key, value]) => (
