174e700e5ec6a31b48eb015e90db36e2995e6c91
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2023-04-19 12:20:02 +0000

    Migrate GlobalInfo

--- src/components/GlobalInfo.tsx
+++ src/components/GlobalInfo.tsx
@@ -1,8 +1,6 @@
-import { Form, Link, useLocation, useNavigate, useTransition } from "@remix-run/react"
+import { Form, Link, useLocation, useNavigate, useNavigation } from "@remix-run/react"
 import { dateTimeFormatShort } from "~/util"
 import { useData } from "../contexts/DataContext"
-import { Spacer } from "./Spacer"
-import styled from "styled-components"
 import { useEffect, useState } from "react"
 import { RevisionSelect } from "./RevisionSelect"
 import { Refresh as RefreshIcon, Folder as FolderIcon } from "@styled-icons/material"
@@ -12,7 +10,7 @@ const analyzingTitle = "Analyzing | Git Truck"
 
 export function GlobalInfo() {
   const { analyzerData, repo } = useData()
-  const transitionState = useTransition()
+  const transitionState = useNavigation()
 
   const location = useLocation()
   const navigate = useNavigate()
@@ -34,16 +32,18 @@ export function GlobalInfo() {
   }, [transitionState.state])
 
   return (
-    <div className="box">
+    <div className="box flex flex-col gap-2">
       <div className="grid w-full grid-cols-[auto_1fr] place-items-center gap-2">
-        <StyledLink to=".." title="See all repositories">
+        <Link
+          className="flex items-center gap-2 text-gray-400 hover:text-gray-600"
+          to=".."
+          title="See all repositories"
+        >
           <FolderIcon display="inline-block" height="1rem" />
-          <StyledP>See more repositories</StyledP>
-        </StyledLink>
+          <p>See more repositories</p>
+        </Link>
       </div>
-      <Spacer />
       <h2 className="box__title">{repo.name}</h2>
-      <Spacer />
       <RevisionSelect
         key={repo.currentHead}
         disabled={isAnalyzing}
@@ -52,16 +52,18 @@ export function GlobalInfo() {
         headGroups={repo.refs}
         analyzedHeads={repo.analyzedHeads}
       />
-      <Spacer />
-      <strong>Analyzed: </strong>
-      <span>{dateTimeFormatShort(analyzerData.lastRunEpoch)}</span>
-      <Spacer />
-      <strong>As of commit: </strong>
-      <span title={analyzerData.commit.message ?? "No commit message"}>{analyzerData.commit.hash.slice(0, 7)}</span>
-      <Spacer />
-      <strong>Files analyzed: </strong>
-      <span>{analyzerData.commit.fileCount ?? 0}</span>
-      <Spacer xl />
+      <div>
+        <strong>Analyzed: </strong>
+        <span>{dateTimeFormatShort(analyzerData.lastRunEpoch)}</span>
+      </div>
+      <div>
+        <strong>As of commit: </strong>
+        <span title={analyzerData.commit.message ?? "No commit message"}>{analyzerData.commit.hash.slice(0, 7)}</span>
+      </div>
+      <div>
+        <strong>Files analyzed: </strong>
+        <span>{analyzerData.commit.fileCount ?? 0}</span>
+      </div>
       <Form
         method="post"
         action={location.pathname}
@@ -78,19 +80,3 @@ export function GlobalInfo() {
     </div>
   )
 }
-
-const StyledP = styled.p`
-  margin-left: var(--unit);
-`
-
-const StyledLink = styled(Link)`
-  display: inline-flex;
-  margin-right: var(--unit);
-  text-decoration: none;
-  color: hsl(0, 0%, 60%);
-  font-size: 0.9em;
-  align-items: center;
-  &:hover {
-    color: hsl(0, 0%, 20%);
-  }
-`
