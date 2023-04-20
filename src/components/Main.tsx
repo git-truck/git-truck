fe9b2f16f6ac2295696550e6139bea855a015884
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2023-04-20 13:54:17 +0000

    Extract breadcrumb component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,7 +4,7 @@ import { usePath } from "~/contexts/PathContext"
 import { useComponentSize } from "../hooks"
 import { Chart } from "./Chart"
 import { Fullscreen as FullscreenIcon, CloseFullscreen as CloseFullscreenIcon } from "@styled-icons/material"
-import type { Dispatch, SetStateAction } from "react"
+import { Dispatch, SetStateAction, useMemo } from "react"
 import { Icon } from "@mdi/react"
 import { mdiHome, mdiChevronRight } from "@mdi/js"
 
@@ -41,66 +41,11 @@ interface MainProps {
 
 export function Main({ fullscreenState: [isFullscreen, setIsFullscreen] }: MainProps) {
   const [ref, size] = useComponentSize()
-  const { path, setPath } = usePath()
-  const { repo } = useData()
-
-  let temppath = path
-  let paths: [string, string][] = []
-
-  for (let i = 0; i < 8; i++) {
-    if (temppath === "") {
-      break
-    }
-    const idx = temppath.lastIndexOf("/")
-    paths.push([temppath.substring(idx + 1), temppath])
-    temppath = temppath.substring(0, idx)
-  }
-
-  if (temppath !== "") {
-    paths = paths.slice(0, paths.length - 1)
-    paths.push(["...", ""])
-    paths.push([repo.name, repo.name])
-  }
 
   return (
     <MainRoot>
       <TopBar>
-        <div className="flex items-center gap-1">
-          {paths.length > 1
-            ? paths.reverse().map(([name, p], i) => {
-                if (p === "" || i === paths.length - 1)
-                  if (p === "")
-                    return (
-                      <>
-                        <span className="font-bold" key={p}>
-                          {name}
-                        </span>
-                        <Icon path={mdiChevronRight} size={1} />
-                      </>
-                    )
-                  else
-                    return (
-                      <span className="font-bold" key={p}>
-                        {name}
-                      </span>
-                    )
-                else
-                  return (
-                    <>
-                      <button
-                        className="card flex flex-row gap-2 py-1 px-2 font-bold hover:opacity-70"
-                        key={p}
-                        onClick={() => setPath(p)}
-                      >
-                        {i === 0 ? <Icon path={mdiHome} size={1} /> : null}
-                        {name}
-                      </button>
-                      <Icon path={mdiChevronRight} size={1} />
-                    </>
-                  )
-              })
-            : null}
-        </div>
+        <Breadcrumb />
         <button className="btn--icon" onClick={() => setIsFullscreen((isFullscreen) => !isFullscreen)}>
           {isFullscreen ? <CloseFullscreenIcon height="1.5em" /> : <FullscreenIcon height="1.5em" />}
         </button>
@@ -109,5 +54,68 @@ export function Main({ fullscreenState: [isFullscreen, setIsFullscreen] }: MainP
         <Chart size={size} />
       </ChartWrapper>
     </MainRoot>
+  )
+}
+
+function Breadcrumb() {
+  const { repo } = useData()
+  const { path, setPath } = usePath()
+  let temppath = path
+  const paths = useMemo<[string, string][]>(() => {
+    let paths: [string, string][] = []
+    for (let i = 0; i < 8; i++) {
+      if (temppath === "") {
+        break
+      }
+      const idx = temppath.lastIndexOf("/")
+      paths.push([temppath.substring(idx + 1), temppath])
+      temppath = temppath.substring(0, idx)
+    }
+
+    if (temppath !== "") {
+      paths = paths.slice(0, paths.length - 1)
+      paths.push(["...", ""])
+      paths.push([repo.name, repo.name])
+    }
+    return paths.reverse()
+  }, [path, repo.name])
+
+  return (
+    <div className="flex items-center gap-1">
+      {paths.length > 1
+        ? paths.map(([name, p], i) => {
+            if (p === "" || i === paths.length - 1)
+              if (p === "")
+                return (
+                  <>
+                    <span className="font-bold" key={p}>
+                      {name}
+                    </span>
+                    <Icon path={mdiChevronRight} size={1} />
+                  </>
+                )
+              else
+                return (
+                  <span className="font-bold" key={p}>
+                    {name}
+                  </span>
+                )
+            else
+              return (
+                <>
+                  <button
+                    className="card flex flex-row gap-2 px-2 py-1 font-bold hover:opacity-70"
+                    key={p}
+                    onClick={() => setPath(p)}
+                  >
+                    {i === 0 ? <Icon path={mdiHome} size={1} /> : null}
+                    {name}
+                  </button>
+                  <Icon path={mdiChevronRight} size={1} />
+                </>
+              )
+          })
+        : null}
+    </div>
   )
 }
