bb20bbf3efa024671a564bea4fac806bc66a5125
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2023-04-23 20:23:45 +0000

    Remove styled components

--- src/entry.client.tsx
+++ src/entry.client.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from "react"
-import { hydrate } from "react-dom"
 import { RemixBrowser } from "@remix-run/react";
+import { startTransition, StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
 
-hydrate(
-  <StrictMode>
-    <RemixBrowser />
-  </StrictMode>,
-  document
-)
+function hydrate() {
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <StrictMode>
+        <RemixBrowser />
+      </StrictMode>
+    );
+  });
+}
+
+if (typeof requestIdleCallback === "function") {
+  requestIdleCallback(hydrate);
+} else {
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  setTimeout(hydrate, 1);
+}
