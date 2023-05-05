977cf631bc1f1d8cad87aea81bd8f768e2750114
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2023-05-05 21:28:58 +0200

    Enable HMR enabled dev server

--- remix.config.mjs
+++ remix.config.mjs
@@ -5,6 +5,7 @@ export default {
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
+    unstable_dev: true,
   },
   ignoredRouteFiles: ["**/.*", "components/*"],
   serverDependenciesToBundle: "all",
