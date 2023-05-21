8add00bf5f7cbe1adb9ab7a844366e7f73a8c3f1
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2023-05-21 18:01:38 +0200

    Add Last changed size metric

--- src/metrics/size-metric.ts
+++ src/metrics/size-metric.ts
@@ -1,9 +1,9 @@
 export const SizeMetric = {
   FILE_SIZE: "File size",
   TRUCK_FACTOR: "Truck factor",
-  MOST_COMMITS: "Number of commits",
+  MOST_COMMITS: "Commits",
+  LAST_CHANGED: "Last changed",
   EQUAL_SIZE: "Equal",
-  RANDOM: "Random",
 }
 
 export type SizeMetricType = keyof typeof SizeMetric
