a78d633720820722f0a9f7627b923caaafd5092d
Dawid Wozniak <dawidwozniak0805@gmail.com>
2023-07-17 03:22:43 +0200

    Add accordion

---
+++ src/components/accordion/AccordionItem.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react"
+import type { AccordionData } from "./Accordion"
+import clsx from "clsx"
+
+function AccordionItem({
+    data,
+    isOpen,
+    btnOnClick,
+    titleLabels
+}: {
+    data: AccordionData
+    isOpen: boolean
+    btnOnClick: () => void
+    titleLabels?: boolean
+}) {
+    const [ height, setHeight ] = useState("")
+
+    useEffect(() => {
+        if (isOpen) {
+            setHeight("auto")
+        } else {
+            setHeight("0")
+        }
+    }, [ isOpen ])
+
+    const afterStyle = "after:content-[''] after:shrink-0 after:w-[14px] after:h-[14px] after:ml-auto after:transition-[transform] after:duration-[200ms] after:ease-out after:bg-[length:14px_14px] after:bg-no-repeat after:bg-arrow";
+    return (
+        <li className="border-0 ">
+            <h2 className="w-full m-0">
+                <div className={ clsx("flex items-center b-0 w-full cursor-pointer " + afterStyle, {
+                    "text-[16px] font-semibold": titleLabels,
+                    "text-[14px] font-normal": !titleLabels,
+                    "after:-rotate-180": isOpen,
+                    "after:rotate-0": !isOpen
+                })} onClick={ btnOnClick } title={data.title}>
+                    { data.title }
+                </div>
+            </h2>
+            <ul className="block m-0 p-0 text-sm transition-[height] duration-[200ms] ease-out" style={ { height } }>
+                { isOpen && <div>{ data.content }</div> }
+            </ul>
+        </li>
+    )
+}
+
+export default AccordionItem
