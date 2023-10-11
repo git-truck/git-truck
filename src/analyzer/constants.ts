06ffcdfad8fecfe97b156b6070c7478a593c460a
tjomson <thomaskilbak@gmail.com>
2023-10-11 18:45:45 +0200

    Fix regex issue identified by codeql

--- src/analyzer/constants.ts
+++ src/analyzer/constants.ts
@@ -1,6 +1,6 @@
 export const emptyGitCommitHash = "4b825dc642cb6eb9a060e54bf8d69288fbee4904"
 export const gitLogRegex =
-  /"author\s+<\|(?<author>.+?)\|>\s+date\s+<\|(?<date>\d+)\|>\s+message\s+<\|(?<message>(?:.|\s)+?)\|>\s+body\s+<\|(?<body>(?:.|\s)*?)\|>\s+hash\s+<\|(?<hash>.+?)\|>"\s*(?<contributions>(?:(?:\d+|-)\s+(?:\d+|-)\s+.+\s?)*)/gm
+  /"author\s+<\|(?<author>.+?)\|>\s+date\s+<\|(?<date>\d+)\|>\s+message\s+<\|(?<message>(?:.|\n)+?)\|>\s+body\s+<\|(?<body>(?:.|\n)*?)\|>\s+hash\s+<\|(?<hash>.+?)\|>"\s*(?<contributions>(?:(?:\d+|-)\s+(?:\d+|-)\s+.+\s?)*)/gm
 export const contribRegex = /(?<insertions>\d+|-)\s+(?<deletions>\d+|-)\s+(?<file>.+)/gm
 
 export const OPTIONS_LOCAL_STORAGE_KEY = "options"
