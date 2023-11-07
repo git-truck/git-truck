efd427a386b6fe640a4d4787136ba179f871cddc
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2023-11-07 23:01:02 +0100

    Update chart rendering
    Update treemap and circle text offsets

--- src/const.ts
+++ src/const.ts
@@ -1,12 +1,14 @@
-export const treemapPaddingTop = 20
-export const treemapNodeBorderRadius = 8
-export const treemapTreeTextOffsetX = 13
-export const treemapTreeTextOffsetY = 15
-export const treemapBlobTextOffsetX = 10
-export const treemapBlobTextOffsetY = 16
-export const bubblePadding = 10
-export const circleTreeTextOffsetY = 0
-export const circleBlobTextOffsetY = 10
-export const searchMatchColor = "red"
 export const estimatedLetterWidth = 10
 export const estimatedLetterHeightForDirText = 14
+
+export const treemapPaddingTop = 20
+export const treemapNodeBorderRadius = 8
+export const treemapTreeTextOffsetX = treemapNodeBorderRadius
+export const treemapTreeTextOffsetY = 3
+export const treemapBlobTextOffsetX = 4
+export const treemapBlobTextOffsetY = 3
+
+export const bubblePadding = 10
+export const circleTreeTextOffsetY = estimatedLetterHeightForDirText - 1
+export const circleBlobTextOffsetY = estimatedLetterHeightForDirText
+export const searchMatchColor = "red"
