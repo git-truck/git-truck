2a22d8b6463cad36e6d90ae810e38493742ec8ff
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2023-11-10 01:48:00 +0100

    Fix race condition when browser opens prematurely
    Fixes #673
    
    See this breaking change from Remix for context:
    https://github.com/remix-run/remix/pull/7229

--- src/cli.ts
+++ src/cli.ts
@@ -1,16 +1,19 @@
 #!/usr/bin/env node
 
+import express from "express"
+import compression from "compression"
+import morgan from "morgan"
+import { createRequestHandler } from "@remix-run/express"
 import path from "path"
 import pkg from "../package.json"
 import open from "open"
 import latestVersion from "latest-version"
 import { GitCaller } from "./analyzer/git-caller.server"
 import { getArgsWithDefaults, parseArgs } from "./analyzer/args.server"
-import { getPathFromRepoAndHead } from "./util"
-// import { createApp } from "@remix-run/serve"
-import { semverCompare } from "./util"
+import { semverCompare, getPathFromRepoAndHead } from "./util"
 import { describeAsyncJob, getDirName } from "./analyzer/util.server"
 import { log, setLogLevel } from "./analyzer/log.server"
+import type { NextFunction } from "express-serve-static-core"
 
 async function main() {
   const args = parseArgs()
@@ -65,46 +68,105 @@ for usage instructions.`)
 
   // Serve application build
 
-  process.argv[2] = path.join(__dirname, "build", "index.js")
-  void new Promise(() => require("@remix-run/serve/dist/cli.js"))
+  const onListen = async () => {
+    const url = `http://localhost:${port}`
 
-  const url = `http://localhost:${port}`
+    const [extension, extensionError] = await describeAsyncJob({
+      job: async () => {
+        // If CWD or path argument is a git repo, go directly to that repo in the visualizer
+        if (await GitCaller.isGitRepo(options.path)) {
+          const repoName = getDirName(options.path)
+          if (repoName) {
+            const currentHead = await GitCaller._getRepositoryHead(options.path)
+            return `/${getPathFromRepoAndHead(repoName, currentHead)}`
+          } else return ""
+        }
+      },
+      beforeMsg: "Checking for git repo",
+      afterMsg: "Done checking for git repo",
+      errorMsg: "Failed to check for git repo",
+    })
 
-  const [extension, extensionError] = await describeAsyncJob({
-    job: async () => {
-      // If CWD or path argument is a git repo, go directly to that repo in the visualizer
-      if (await GitCaller.isGitRepo(options.path)) {
-        const repoName = getDirName(options.path)
-        if (repoName) {
-          const currentHead = await GitCaller._getRepositoryHead(options.path)
-          return `/${getPathFromRepoAndHead(repoName, currentHead)}`
-        } else return ""
-      }
-    },
-    beforeMsg: "Checking for git repo",
-    afterMsg: "Done checking for git repo",
-    errorMsg: "Failed to check for git repo",
-  })
-
-  if (extensionError) {
-    console.error(extensionError)
-  }
-
-  if (process.env.NODE_ENV !== "development") {
-    const openURL = url + (extension ?? "")
-    let err: Error | null = null
-
-    if (!args.headless) {
-      log.debug(`Opening ${openURL}`)
-      ;[, err] = await describeAsyncJob({
-        job: () => open(openURL),
-        beforeMsg: "Opening Git Truck in your browser",
-        afterMsg: `Succesfully opened Git Truck in your browser`,
-        errorMsg: `Failed to open Git Truck in your browser. To continue, open this link manually:\n\n${openURL}`,
-      })
+    if (extensionError) {
+      console.error(extensionError)
     }
-    if (!err) console.log(`\nApplication available at ${url}\n`)
+
+    if (process.env.NODE_ENV !== "development") {
+      const openURL = url + (extension ?? "")
+
+      if (!args.headless) {
+        log.debug(`Opening ${openURL}`)
+        await describeAsyncJob({
+          job: () => open(openURL),
+          beforeMsg: "Opening Git Truck in your browser",
+          afterMsg: "Opened Git Truck in your browser",
+          errorMsg: `Failed to open Git Truck in your browser. To continue, open this link manually:\n\n${openURL}\n`,
+        })
+      } else {
+        console.log(`Application available at ${url}`)
+      }
+    }
   }
+
+  describeAsyncJob({
+    job: async () => {
+      const app = createApp(
+        "./build/index.js",
+        process.env.NODE_ENV ?? "production",
+        "/build",
+        path.join(__dirname, "public", "build"),
+      )
+
+      const server = process.env.HOST ? app.listen(port, process.env.HOST, onListen) : app.listen(port, onListen)
+      ;["SIGTERM", "SIGINT"].forEach((signal) => {
+        process.once(signal, () => server?.close(console.error))
+      })
+    },
+    beforeMsg: "Starting app",
+    afterMsg: "App started",
+    errorMsg: "Failed to start app",
+  })
 }
 
 main()
+
+function createApp(
+  buildPath: string,
+  mode = "production",
+  publicPath = "/build/",
+  assetsBuildDirectory = "public/build/",
+) {
+  const app = express()
+
+  app.disable("x-powered-by")
+
+  // @ts-expect-error This error is wrong, the types are incorrect
+  app.use(compression())
+
+  // @ts-expect-error This error is wrong, the types are incorrect
+  app.use(publicPath, express.static(assetsBuildDirectory, { immutable: true, maxAge: "1y" }))
+
+  // @ts-expect-error This error is wrong, the types are incorrect
+  app.use(express.static("public", { maxAge: "1h" }))
+
+  if (mode === "development") {
+    // @ts-expect-error This error is wrong, the types are incorrect
+    app.use(morgan("dev"))
+  }
+
+  let requestHandler: ReturnType<typeof createRequestHandler> | undefined
+  app.all("*", async (req, res, next) => {
+    try {
+      if (!requestHandler) {
+        const build = await import(buildPath)
+        requestHandler = createRequestHandler({ build, mode })
+      }
+
+      return await requestHandler(req, res, next as NextFunction)
+    } catch (error) {
+      next?.(error)
+    }
+  })
+
+  return app
+}
