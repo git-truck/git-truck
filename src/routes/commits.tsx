f1a20a6a5e228a06b21f313f81044dc9f7d00032
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2023-11-14 22:28:40 +0100

    Fix type from breaking change

--- src/routes/commits.tsx
+++ src/routes/commits.tsx
@@ -1,15 +1,15 @@
-import type { LoaderArgs } from "@remix-run/node"
-import { GitCaller } from "~/analyzer/git-caller.server";
-import { gatherCommitsFromGitLog } from "~/analyzer/hydrate.server";
-import type { GitLogEntry } from "~/analyzer/model";
+import type { LoaderFunctionArgs } from "@remix-run/node"
+import { GitCaller } from "~/analyzer/git-caller.server"
+import { gatherCommitsFromGitLog } from "~/analyzer/hydrate.server"
+import type { GitLogEntry } from "~/analyzer/model"
 
-export const loader = async ({ request }: LoaderArgs) => {
-    const url = new URL(request.url);
-    const commitHashes = url.searchParams.get("commits")?.split(",") ?? []
-    const commits = new Map<string, GitLogEntry>()
-    const gitShowResult = await GitCaller.getInstance().gitShow(commitHashes)
-    gatherCommitsFromGitLog(gitShowResult, commits, false)
-    const array: GitLogEntry[] = []
-    for (const [_, commit] of commits) array.push(commit)
-    return array
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url)
+  const commitHashes = url.searchParams.get("commits")?.split(",") ?? []
+  const commits = new Map<string, GitLogEntry>()
+  const gitShowResult = await GitCaller.getInstance().gitShow(commitHashes)
+  gatherCommitsFromGitLog(gitShowResult, commits, false)
+  const array: GitLogEntry[] = []
+  for (const [_, commit] of commits) array.push(commit)
+  return array
 }
