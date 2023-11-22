6846d93efd6e79ca583d637377a4f252fe3bf194
tjomson <thomaskilbak@gmail.com>
2023-11-22 09:29:20 +0100

    Broken migration

--- src/root.tsx
+++ src/root.tsx
@@ -11,40 +11,13 @@ import {
   useRouteError
 } from "@remix-run/react"
 
-import varsStyles from "~/styles/vars.css"
-import indexStyles from "~/styles/animations.css"
+import "~/styles/vars.css"
 import { Code } from "./components/util"
-import tailwindStylesheet from "~/tailwind.css"
+import "~/tailwind.css"
 import { ThemeProvider, cn, usePrefersLightMode } from "./styling"
 
 export const meta: MetaFunction = () => {
   return [{ title: "Git Truck" }]
-}
-
-export function links() {
-  return [
-    ...[varsStyles, indexStyles, tailwindStylesheet].map((x) => ({
-      rel: "stylesheet",
-      href: x
-    })),
-    {
-      rel: "favicon",
-      type: "image/x-icon",
-      href: "favicon.ico"
-    },
-    {
-      rel: "preconnect",
-      href: "https://fonts.googleapis.com"
-    },
-    {
-      rel: "preconnect",
-      href: "https://fonts.gstatic.com"
-    },
-    {
-      rel: "stylesheet",
-      href: "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;900&family=Roboto+Mono:wght@400;700&display=swap"
-    }
-  ]
 }
 
 export default function App() {
