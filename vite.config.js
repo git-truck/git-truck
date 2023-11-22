690712ca6ebe99c4f194e501dba2439ffcfb0296
tjomson <thomaskilbak@gmail.com>
2023-11-22 09:29:20 +0100

    Broken migration

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { installGlobals } from "@remix-run/node";
+
+installGlobals();
+
+export default defineConfig({
+  plugins: [remix(), tsconfigPaths()],
+});
