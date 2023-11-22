397423fba87dbd422691c5592d008cbf633b21eb
tjomson <thomaskilbak@gmail.com>
2023-11-22 12:59:16 +0100

    Differentiate authors by color in "single author" visualization (#702)
    Fixes #574

--- src/metrics/singleAuthor.ts
+++ src/metrics/singleAuthor.ts
@@ -3,35 +3,26 @@ import type { PointLegendData } from "~/components/legend/PointLegend"
 import { PointInfo } from "~/components/legend/PointLegend"
 import type { AuthorshipType, MetricCache } from "./metrics"
 
-export function setDominanceColor(blob: HydratedGitBlobObject, cache: MetricCache, authorshipType: AuthorshipType) {
-  const dominatedColor = "#b91c1c"
-  const defaultColor = "#fecaca"
-  const nocreditColor = "#14b8a6"
-
+export function setDominanceColor(blob: HydratedGitBlobObject, cache: MetricCache, authorshipType: AuthorshipType, authorColors: Map<string, `#${string}`>) {
+  const multipleAuthorsColor = "#e0e0e0"
+  const noAuthorsColor = "#404040"
+  
   const authorUnion = blob.unionedAuthors?.[authorshipType] ?? {}
-
-  let creditsum = 0
-  for (const [, val] of Object.entries(authorUnion)) {
-    creditsum += val
-  }
-
+  
   const legend = cache.legend as PointLegendData
-
-  if (creditsum === 0) {
-    legend.set("No authors", new PointInfo(nocreditColor, 0))
-    cache.colormap.set(blob.path, nocreditColor)
-    return
-  }
-
-  if (!authorUnion) throw Error("No unioned authors found")
-  switch (Object.keys(authorUnion).length) {
+  const authors = Object.keys(authorUnion)
+  switch (authors.length) {
+    case 0:
+      legend.set("No authors", new PointInfo(noAuthorsColor, 0))
+      cache.colormap.set(blob.path, noAuthorsColor)
+      return
     case 1:
-      legend.set("Single author", new PointInfo(dominatedColor, 2))
-      cache.colormap.set(blob.path, dominatedColor)
+      const color = authorColors.get(authors[0]) ?? noAuthorsColor
+      legend.set(authors[0], new PointInfo(color, 2))
+      cache.colormap.set(blob.path, color)
       return
     default:
-      legend.set("Multiple authors", new PointInfo(defaultColor, 1))
-      cache.colormap.set(blob.path, defaultColor)
-      return
+      legend.set("Multiple authors", new PointInfo(multipleAuthorsColor, 1))
+      cache.colormap.set(blob.path, multipleAuthorsColor)
   }
 }
