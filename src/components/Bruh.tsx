3cd7f78ba65a0777d007e86bdcabf3a87f0b4d89
tjomson <thomaskilbak@gmail.com>
2023-11-27 12:09:39 +0100

    Refactor circle to component

--- src/components/Bruh.tsx
+++ src/components/Bruh.tsx
@@ -1,12 +1,12 @@
 import * as pixi from 'pixi.js';
 import { Stage, Graphics, Text } from '@pixi/react';
-import { Fragment } from 'react';
+import { Spring } from 'react-spring';
 import type { HierarchyCircularNode, HierarchyRectangularNode } from "d3-hierarchy"
 import type { HydratedGitObject } from '~/analyzer/model';
+import { useClickedObject } from '~/contexts/ClickedContext';
 
-export function Bruh(props: {nodes: HierarchyRectangularNode<HydratedGitObject>[] | HierarchyCircularNode<HydratedGitObject>[]}) {
-
-  const art = (g: pixi.Graphics, node: HierarchyRectangularNode<HydratedGitObject> | HierarchyCircularNode<HydratedGitObject>) => {
+function CircleNode(props: {node: HierarchyCircularNode<HydratedGitObject>, setClickedObject: (clicked: HydratedGitObject) => void}) {
+  const draw = (g: pixi.Graphics, node: HierarchyCircularNode<HydratedGitObject>) => {
     g.clear();
     const circleDatum = node as HierarchyCircularNode<HydratedGitObject>
     if (circleDatum.data.type === "blob") {
@@ -18,21 +18,29 @@ export function Bruh(props: {nodes: HierarchyRectangularNode<HydratedGitObject>[
     if (circleDatum.data.type === "blob") g.endFill();
     g.interactive = true
     g.hitArea = new pixi.Circle(circleDatum.x, circleDatum.y, circleDatum.r)
-    g.addEventListener("mouseover", (e) => console.log("hover", node.data.path))
+    g.addEventListener("click", (e) => {
+      props.setClickedObject(node.data)
+      console.log("hover", node.data.path)
+    })
     console.log("draw", node.data.path)
   }
+  return (
+    <>
+      <Graphics draw={(g) => draw(g, props.node)} />
+      <Text text={props.node.data.name} x={props.node.x} y={props.node.y} style={new pixi.TextStyle({fontSize: 10, align: "center"})} />
+    </>
+  )
+}
+
+export function Bruh(props: {nodes: HierarchyRectangularNode<HydratedGitObject>[] | HierarchyCircularNode<HydratedGitObject>[]}) {
+  const { setClickedObject } = useClickedObject()
 
   return (
     <Stage width={600} height={800} options={{ backgroundColor: 0xffffff, antialias: true }}>
-        {props.nodes.map((node) => {
-            const circleDatum = node as HierarchyCircularNode<HydratedGitObject>
-            return (
-                <Fragment key={node.data.path}>
-                    <Graphics draw={(g) => art(g, node)} />
-                    <Text text={node.data.name} x={circleDatum.x} y={circleDatum.y} style={new pixi.TextStyle({fontSize: 10, align: "center"})} />
-                </Fragment>
-            )
-        })}
+      {props.nodes.map((node) => {
+        const circleDatum = node as HierarchyCircularNode<HydratedGitObject>
+        return <CircleNode key={node.data.path} node={circleDatum} setClickedObject={setClickedObject}/> 
+      })}
     </Stage>
   );
 }
