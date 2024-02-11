44f841f882a31c4566821027ad138540ce774113
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2024-02-11 22:40:23 +0100

    Run eslint in workflows Fixes #727

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: false,
+  printWidth: 120,
+  trailingComma: "none",
+  plugins: ["prettier-plugin-tailwindcss"]
+}
+
+export default config
