3a1b8755566a9a0c12a56546c04ebdce3fa44d38
tjomson <thomaskilbak@gmail.com>
2024-02-26 20:06:52 +0100

    Minor regex fixes

--- src/analyzer/constants.ts
+++ src/analyzer/constants.ts
@@ -1,6 +1,6 @@
 export const emptyGitCommitHash = "4b825dc642cb6eb9a060e54bf8d69288fbee4904"
 export const gitLogRegex =
-  /"author\s+<\|(?<author>.+?)\|>\s+date\s+<\|(?<date>\d+)\|>\s+message\s+<\|(?<message>(?:.|\n|\r|\u2028)+?)\|>\s+body\s+<\|(?<body>(?:.|\n|\r|\u2028)*?)\|>\s+hash\s+<\|(?<hash>.+?)\|>"\s*(?<contributions>(?:(?:\d+|-)\s+(?:\d+|-)\s+.+\s?)*)/gm
+  /"author\s+<\|(?<author>.*?)\|>\s+date\s+<\|(?<date>\d+)\|>\s+message\s+<\|(?<message>(?:.|\s)*?)\|>\s+body\s+<\|(?<body>(?:.|\s)*?)\|>\s+hash\s+<\|(?<hash>.+?)\|>"\s*(?<contributions>(?:(?:\d+|-)\s+(?:\d+|-)\s+.+\s?)*)/ugm
 export const contribRegex = /(?<insertions>\d+|-)\s+(?<deletions>\d+|-)\s+(?<file>.+)/gm
 export const treeRegex = /^\S+? (?<type>\S+) (?<hash>\S+)\s+(?<size>\S+)\s+(?<path>.+)/gm
 export const OPTIONS_LOCAL_STORAGE_KEY = "options"
