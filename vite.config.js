53f28e2eb71ab4871a6754422aa1f7cf0806a210
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2024-04-18 15:56:16 +0200

    Add vite-plugin-cjs-interop and update remix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { installGlobals } from "@remix-run/node";
+import { cjsInterop } from "vite-plugin-cjs-interop";
 
 installGlobals();
 
@@ -12,9 +13,15 @@ export default defineConfig({
   plugins: [
     remix({
       appDirectory: "src",
-      serverBuildPath: "build/index.js",
       serverModuleFormat: "cjs"
     }),
-    tsconfigPaths()
+    tsconfigPaths(),
+    cjsInterop({
+      dependencies: [
+        "@mdi/react",
+        "react-use",
+        "react-konami-code"
+      ]
+    })
   ]
 })
