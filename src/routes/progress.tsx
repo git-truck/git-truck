7c1e4b96bf1cd75f169df7e35be791bc57f730fb
Thomas Kilbak <thomaskilbak@gmail.com>
2024-04-24 11:39:59 +0200

    Bandaid to progress exceeding 100%

--- src/routes/progress.tsx
+++ src/routes/progress.tsx
@@ -15,14 +15,21 @@ export const loader = async ({ request }: LoaderFunctionArgs): Promise<ProgressR
   if (!repo || !branch) return defaultResponse
   const instance = InstanceManager.getInstance(repo, branch)
   if (!instance) return defaultResponse
-  let progressPercentage = instance.progress.reduce((acc, curr) => acc+curr, 0)
+  let progressPercentage = instance.progress.reduce((acc, curr) => acc + curr, 0)
   let status = instance.analyzationStatus
-  while (instance.prevProgress.str === status+progressPercentage || instance.prevProgress.timestamp + 1000 > Date.now()) {
+  while (
+    instance.prevProgress.str === status + progressPercentage ||
+    instance.prevProgress.timestamp + 1000 > Date.now()
+  ) {
     await sleep(1000)
-    progressPercentage = instance.totalCommitCount > 0 ? Math.floor((instance.progress.reduce((acc, curr) => acc+curr, 0) / instance.totalCommitCount) * 100) : 0
+    progressPercentage =
+      instance.totalCommitCount > 0
+        ? Math.floor((instance.progress.reduce((acc, curr) => acc + curr, 0) / instance.totalCommitCount) * 100)
+        : 0
+    if (progressPercentage > 100) progressPercentage = 100
     status = instance.analyzationStatus
   }
-  instance.prevProgress.str = status+progressPercentage
+  instance.prevProgress.str = status + progressPercentage
   instance.prevProgress.timestamp = Date.now()
   return {
     progress: progressPercentage,
