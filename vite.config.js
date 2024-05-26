57175efc6d7f93af1c510af3830a17db6c055d19
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2024-05-26 18:28:53 +0200

    Remove cjsInterop

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { vitePlugin as remix } from "@remix-run/dev"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 import { installGlobals } from "@remix-run/node"
-import { cjsInterop } from "vite-plugin-cjs-interop"
 
 installGlobals()
 
@@ -15,9 +14,6 @@ export default defineConfig({
       appDirectory: "src",
       serverModuleFormat: "esm"
     }),
-    tsconfigPaths(),
-    cjsInterop({
-      dependencies: ["@mdi/react", "react-konami-code"]
-    })
+    tsconfigPaths()
   ]
 })
