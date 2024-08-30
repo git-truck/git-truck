47ebf1d61ae92b6bc0b295f135af8f2ef1eaca68
tjomson <thomaskilbak@gmail.com>
2024-08-30 16:00:33 +0200

    Maybe fix macos e2e

--- e2e/features/navigate-to-a-repository.spec.ts
+++ e2e/features/navigate-to-a-repository.spec.ts
@@ -1,6 +1,12 @@
 import { test, expect } from "@playwright/test"
+import { resolve } from "path"
+import { existsSync, promises as fs } from "fs"
+import os from "os"
 
 test("navigate to a repository", async ({ page }) => {
+  const dir = resolve(os.tmpdir(), "git-truck-cache")
+  if (!existsSync(dir)) return
+  await fs.rm(dir, { recursive: true, force: true })
   await page.goto("/")
 
   // Expect a title "to contain" a substring.
@@ -13,9 +19,11 @@ test("navigate to a repository", async ({ page }) => {
   // Select an option from a <select> dropdown.
   await page.getByTestId("revision-select-git-truck").selectOption("main")
   // Click the analyze button.
-  await page.getByTitle("View git-truck", {
-    exact: true
-  }).click()
+  await page
+    .getByTitle("View git-truck", {
+      exact: true
+    })
+    .click()
 
   await page.waitForURL("/git-truck/main")
   await page.waitForSelector("text=More repositories")
