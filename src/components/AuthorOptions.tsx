git-truck/git-visual

npx codemod remix/2/react-router/upgrade && npm install

src/components/AuthorOptions.tsx
@@ -1,6 +1,6 @@
 import { mdiAccountMultiple, mdiDiceMultipleOutline } from "@mdi/js"
 import Icon from "@mdi/react"
-import { useNavigation, useSubmit } from "@remix-run/react"
+import { useNavigation, useSubmit } from "react-router";
 import { useState } from "react"
 import { Slider, Rail, Handles, Tracks, Ticks, SliderItem } from "react-compound-slider"
 import { useData } from "~/contexts/DataContext"

git-truck/git-truck

src/components/AuthorOptions.tsx
@@ -1,6 +1,6 @@
 import { mdiAccountMultiple, mdiDiceMultipleOutline } from "@mdi/js"
 import Icon from "@mdi/react"
-import { Form, useNavigation } from "@remix-run/react"
+import { Form, useNavigation } from "react-router";
 import { useState } from "react"
 import { Slider, Rail, Handles, Tracks, Ticks, SliderItem } from "react-compound-slider"
 import { useData } from "~/contexts/DataContext"