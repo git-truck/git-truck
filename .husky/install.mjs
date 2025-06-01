ff354419c9c90b4992a2f48ba925e657579f0f4e
nimrossum <1959615+nimrossum@users.noreply.github.com>
2025-06-02 00:22:39 +0200

    Cache playwright browsers (#804)

---
+++ .husky/install.mjs
@@ -0,0 +1,8 @@
+import { install } from "husky"
+
+// Skip Husky install in production and CI
+if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
+  process.exit(0)
+}
+
+install()
