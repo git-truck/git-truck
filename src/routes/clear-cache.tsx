72a31305e7e478fc826a6f2dfa018992adc27f3e
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2025-07-25 21:33:35 +0200

    Refactor ClearCache component to include GitTruckInfo and enhance UI with warning message

--- src/routes/clear-cache.tsx
+++ src/routes/clear-cache.tsx
@@ -1,11 +1,15 @@
 import { mdiDeleteForever } from "@mdi/js"
 import Icon from "@mdi/react"
-import { redirect, useFetcher, useLocation, useNavigation } from "react-router"
+import { Link, redirect, useFetcher, useLoaderData, useLocation, useNavigation } from "react-router"
 import DB from "~/analyzer/DB.server"
 import InstanceManager from "~/analyzer/InstanceManager.server"
 import type { Route } from "./+types/clear-cache"
-import { useEffect, useState } from "react"
 import { cn } from "~/styling"
+import { GitTruckInfo } from "~/components/GitTruckInfo"
+
+export const loader = async ({ context }: Route.LoaderArgs) => {
+  return { versionInfo: context }
+}
 
 export const action = async ({ request }: Route.ActionArgs) => {
   const redirectPath = new URL(request.url).searchParams.get("redirect") as string | null
@@ -34,32 +38,47 @@ export function ClearCacheForm({ redirectPath, className = "" }: { redirectPath?
           event.preventDefault()
           fetcher.submit(event.currentTarget.form)
         }}
-        className={cn("btn", className)}
+        className={cn("btn btn--danger", className)}
         title="Click here if you are experiencing issues"
       >
         <Icon path={mdiDeleteForever} className="hover-swap inline-block h-full" />
-        {isTransitioning ? "Clearing..." : "Clear analyzed results"}
+        {isTransitioning ? "Clearing..." : "Clear all data"}
       </button>
     </fetcher.Form>
   )
 }
 
 export default function ClearCache() {
+  const { versionInfo } = useLoaderData<typeof loader>()
   const navigation = useNavigation()
   const isTransitioning = navigation.state !== "idle"
 
   return (
-    <div className="card m-2 mx-auto flex max-w-xl flex-col items-center justify-center gap-4 text-center">
-      <h1 className="text-2xl font-bold">Clear Cache</h1>
-      <p>This will clear all analyzed results and reset the database cache. Use this if you are experiencing issues.</p>
-      <div className="flex h-full place-items-center gap-2 rounded-lg bg-orange-500/70 p-4 text-white">
-        <Icon path={mdiDeleteForever} className="inline-block h-12" />
-        <div>
-          <span className="font-bold">Warning: </span>
-          <span>Merged authors and hidden files will be deleted.</span>
+    <>
+      <div className="app-container flex flex-col gap-2 p-2">
+        <div className="card">
+          <GitTruckInfo installedVersion={versionInfo.installedVersion} latestVersion={versionInfo.latestVersion} />
+        </div>
+        <div className="card">
+          <h1 className="text-2xl font-bold">Clear Git Truck cache</h1>
+          <p>
+            This will clear all analyzed results and reset the database cache. Only this if you are experiencing issues.
+          </p>
+          <div className="flex h-full place-items-center gap-2 rounded-lg border border-orange-500/70 p-4 text-orange-500">
+            <Icon path={mdiDeleteForever} className="inline-block h-12" />
+            <div>
+              <span className="font-bold">Warning: </span>
+              <span>Merged authors and hidden files will be reset.</span>
+            </div>
+          </div>
+          <div className="flex justify-end gap-2">
+            <Link to="/" className="btn btn--text">
+              Go back
+            </Link>
+            <ClearCacheForm redirectPath="/" />
+          </div>
         </div>
       </div>
-      <ClearCacheForm className="text-orange-700" redirectPath="/" />
-    </div>
+    </>
   )
 }
