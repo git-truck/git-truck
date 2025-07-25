eb013b4ece04465b7831969cc187b1168c1090f5
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2025-07-25 21:32:10 +0200

    Simplify theming

--- src/styling.tsx
+++ src/styling.tsx
@@ -1,38 +1,9 @@
 import { type ClassValue, clsx } from "clsx"
-import type { Dispatch, ReactNode, SetStateAction } from "react"
-import { createContext, useContext } from "react"
-import { useLocalStorage } from "~/hooks"
+import { useMediaQuery } from "~/hooks"
 import { twMerge } from "tailwind-merge"
 
 export const cn = (...args: ClassValue[]) => twMerge(clsx(args))
 
-export const Themes = {
-  LIGHT: "Light",
-  DARK: "Dark"
-} as const
-
-export type Theme = keyof typeof Themes
-
-export const ThemeContext = createContext<[Theme, Dispatch<SetStateAction<Theme | undefined>>]>([
-  "LIGHT",
-  () => {
-    throw new Error("ThemeContext not in the tree")
-  }
-])
-
-export const useTheme = () => useContext(ThemeContext)
-
-export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [theme, setTheme] = useLocalStorage<Theme>("themeType", undefined)
-
-  return (
-    <ThemeContext.Provider value={[theme === undefined ? "LIGHT" : theme, setTheme]}>{children}</ThemeContext.Provider>
-  )
-}
-
 export function usePrefersLightMode() {
-  // const prefersLight = useMQPrefersColorSchemeLight()
-  const [theme] = useTheme()
-
-  return theme === undefined || theme === "LIGHT"
+  return useMediaQuery("(prefers-color-scheme: light)")
 }
