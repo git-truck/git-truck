7d6ba68000763ca06326414886b02a730b25fcb8
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2025-07-25 21:34:04 +0200

    Fix search functionality to use child path instead of name and improve styling of search results

--- src/components/SearchCard.tsx
+++ src/components/SearchCard.tsx
@@ -17,7 +17,7 @@ function findSearchResults(tree: GitTreeObject, searchString: string): SearchRes
   const searchResults: Record<string, GitObject> = {}
   function subTreeSearch(subTree: GitTreeObject) {
     for (const child of subTree.children) {
-      if (child.name.toLowerCase().includes(searchString.toLowerCase()) && searchString) {
+      if (child.path.toLowerCase().includes(searchString.toLowerCase()) && searchString) {
         searchResults[child.path] = child
       }
       if (child.type === "tree") subTreeSearch(child)
@@ -51,75 +51,73 @@ export const SearchCard = memo(function SearchCard() {
   }, [])
 
   return (
-    <>
-      <form
-        className="w-sidepanel absolute top-0 bottom-0 left-1/2 z-10 flex -translate-x-1/2 flex-col gap-2 transition-[width,translate] not-focus-within:has-placeholder-shown:static not-focus-within:has-placeholder-shown:w-min not-focus-within:has-placeholder-shown:translate-x-0"
-        onSubmit={(event) => {
-          event.preventDefault()
-          setSearchText("")
-          event.currentTarget.querySelector("input")?.blur()
-        }}
+    <form
+      className="w-sidepanel absolute top-0 bottom-0 left-1/2 z-10 flex -translate-x-1/2 flex-col gap-2 transition-[width,translate] not-focus-within:has-placeholder-shown:static not-focus-within:has-placeholder-shown:w-min not-focus-within:has-placeholder-shown:translate-x-0"
+      onSubmit={(event) => {
+        event.preventDefault()
+        setSearchText("")
+        event.currentTarget.querySelector("input")?.blur()
+      }}
+    >
+      <button className="hidden min-w-max cursor-pointer peer-placeholder-shown:hidden peer-focus:inline" type="submit">
+        <Icon path={mdiClose} size="1em" className="" />
+      </button>
+      <label
+        className="input min-h-button h-button max-h-button relative flex w-full min-w-0 cursor-pointer flex-row-reverse items-center gap-2 overflow-hidden not-focus-within:has-placeholder-shown:grow-0 not-focus-within:has-placeholder-shown:gap-0"
+        title="Search for a file or folder"
       >
-        <button
-          className="hidden min-w-max cursor-pointer peer-placeholder-shown:hidden peer-focus:inline"
-          type="submit"
-        >
-          <Icon path={mdiClose} size="1em" className="" />
-        </button>
-        <label className="input min-h-button h-button max-h-button relative flex w-full min-w-0 cursor-pointer flex-row-reverse items-center gap-2 overflow-hidden not-focus-within:has-placeholder-shown:grow-0 not-focus-within:has-placeholder-shown:gap-0">
-          <input
-            className="peer w-full grow placeholder-shown:not-focus:w-0 placeholder-shown:not-focus:min-w-0"
-            ref={searchFieldRef}
-            id={id}
-            type="search"
-            placeholder="Search for a file or folder..."
-            value={searchText}
-            onKeyDown={(event) => {
-              if (event.key === "Enter") {
-                event.preventDefault()
-              }
-              if (event.key === "Escape") {
-                event.preventDefault()
-                setSearchText("")
-                setSearchResults({})
-                event.currentTarget.blur()
-              }
-            }}
-            onFocus={() => {
-              if (clickedObject) {
-                setClickedObject(null)
-              }
-            }}
-            onBlur={(event) => {
-              if (event.currentTarget.form?.[id].value.trim() === "") {
-                setSearchText("")
-                setSearchResults({})
-              }
-            }}
-            onChange={(event) => {
-              const value = event.target.value
-              setSearchText(value)
-              startTransition(() => {
-                if (value.trim() === "") setSearchResults({})
-                setSearchResults(findSearchResults(databaseInfo.fileTree, value))
-              })
-            }}
-          />
-          <Icon
-            path={mdiMagnify}
-            size="1em"
-            className="hidden min-w-max peer-placeholder-shown:inline peer-focus:hidden"
-          />
-          <span className="sr-only">Search</span>
-          {isTransitioning || searchText.length > 0 ? (
-            <p className="text-secondary-text dark:text-secondary-text-dark absolute right-8 ml-2 text-sm select-none">
-              {isTransitioning ? "Searching..." : searchText.length > 0 ? `${searchResultsArray.length} results` : null}
-            </p>
-          ) : null}
-        </label>
-        {searchResultsArray.length > 0 ? <SearchResultsList /> : null}
-      </form>
-    </>
+        <input
+          className="peer w-full grow placeholder-shown:not-focus:w-0 placeholder-shown:not-focus:min-w-0"
+          ref={searchFieldRef}
+          id={id}
+          type="search"
+          placeholder="Search for a file or folder..."
+          value={searchText}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              event.preventDefault()
+            }
+            if (event.key === "Escape") {
+              event.preventDefault()
+              setSearchText("")
+              setSearchResults({})
+              event.currentTarget.blur()
+            }
+          }}
+          onFocus={() => {
+            if (clickedObject) {
+              setClickedObject(null)
+            }
+          }}
+          onBlur={(event) => {
+            if (event.currentTarget.form?.[id].value.trim() === "") {
+              setSearchText("")
+              setSearchResults({})
+            }
+          }}
+          onChange={(event) => {
+            const value = event.target.value
+            setSearchText(value)
+            startTransition(() => {
+              if (value.trim() === "") setSearchResults({})
+              setSearchResults(findSearchResults(databaseInfo.fileTree, value))
+            })
+          }}
+        />
+        <Icon
+          path={mdiMagnify}
+          size="1em"
+          className="hidden min-w-max peer-placeholder-shown:inline peer-focus:hidden"
+        />
+        <span className="sr-only">Search</span>
+        {isTransitioning || searchText.length > 0 ? (
+          <p className="text-secondary-text dark:text-secondary-text-dark absolute right-8 ml-2 text-sm select-none">
+            {isTransitioning ? "Searching..." : searchText.length > 0 ? `${searchResultsArray.length} results` : null}
+          </p>
+        ) : null}
+      </label>
+      {searchResultsArray.length > 0 ? <SearchResultsList /> : null}
+    </form>
   )
 })
 
@@ -141,7 +139,7 @@ const SearchResultsList = memo(function SearchResults() {
   }
 
   return (
-    <div className="card bg-tertiary-bg/20 dark:bg-tertiary-bg-dark/20 w-sidepanel relative max-h-1/3 min-h-0 overflow-auto backdrop-blur-lg">
+    <div className="card bg-tertiary-bg/10 dark:bg-tertiary-bg-dark/50 w-sidepanel relative max-h-1/5 min-h-0 overflow-auto backdrop-blur-lg">
       {Object.values(searchResults).map((result) => (
         <button
           className="flex cursor-pointer items-center justify-start gap-2 text-sm font-bold"
@@ -168,7 +166,7 @@ const SearchResultsList = memo(function SearchResults() {
             // />
           )}
           <span className="text-secondary-text dark:hover:text-primary-text-dark hover:text-primary-text dark:text-secondary-text-dark truncate">
-            {result.name}
+            {result.path.split(getSeparator(result.path)).slice(1).join("/") ?? result.path}
           </span>
         </button>
       ))}
