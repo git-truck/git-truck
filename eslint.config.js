93b8eeceb7947cb2d3e26626d75ea0061f6bdd8e
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2025-10-13 18:47:26 +0200

    Delegate unused reporting to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -57,6 +57,8 @@ export default [
       ...jsxA11y.configs.recommended.rules,
       ...importPlugin.configs.recommended.rules,
       ...importPlugin.configs.typescript.rules,
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
     },
   },
   {
