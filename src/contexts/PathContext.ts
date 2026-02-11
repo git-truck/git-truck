d179001c9d0ac502467d8ece2656155789d604fa
Jonas Røssum <1959615+nimrossum@users.noreply.github.com>
2026-02-11 13:19:02 +0100

    Refactor usePath to use nuqs for search parameter handling

--- src/contexts/PathContext.ts
+++ src/contexts/PathContext.ts
@@ -1,14 +1,18 @@
 import { useCallback, useMemo } from "react"
 import { useSearchParams } from "react-router"
 import { useData } from "./DataContext"
+import { useQueryStates } from "nuqs"
+import { viewSearchParamsConfig } from "~/routes/view"
 
 export function usePath(): {
   path: string
   setPath: (newPath: string) => void
 } {
   const data = useData()
-  const [searchParams, setSearchParams] = useSearchParams()
-  const path = useMemo(() => searchParams.get("path") ?? data.repo.repositoryPath, [data.repo.repositoryPath, searchParams])
+  const [searchParams, setSearchParams] = useQueryStates(viewSearchParamsConfig)
+
+  const path = searchParams.path ?? data.repo.repositoryPath
+
   const setPath = useCallback(
     (newPath: string) =>
       setSearchParams((prev) => ({
