8e18865469f197596d72a724e6a5785415afedad
Jonas Røssum <1959615+nimrossum@users.noreply.github.com>
2026-02-24 12:51:27 +0100

    Fix details not being shown when selecting search results

--- src/contexts/ClickedContext.ts
+++ src/contexts/ClickedContext.ts
@@ -1,19 +1,27 @@
 import { useCallback } from "react"
 import type { GitObject } from "../shared/model"
-import { useLocation } from "react-router"
-import { useQueryState } from "nuqs"
-import { viewSearchParamsConfig } from "~/routes/view"
+import { href, useLocation, useMatch, useNavigate } from "react-router"
+import { useQueryStates } from "nuqs"
+import { viewSearchParamsConfig, viewSerializer } from "~/routes/view"
 
 export function useClickedObject() {
   const location = useLocation()
-  const [objectPath, setObjectPath] = useQueryState("objectPath", viewSearchParamsConfig.objectPath)
-  const clickedObject = objectPath ? ((location.state?.clickedObject ?? null) as GitObject | null) : null
+  const [params] = useQueryStates(viewSearchParamsConfig)
+  const clickedObject = params.objectPath ? ((location.state?.clickedObject ?? null) as GitObject | null) : null
+  const navigate = useNavigate()
+  const tabURL = useMatch(href("/view/commits")) ? "/view/commits" : "/view/details"
 
   const setClickedObject = useCallback(
     (object: GitObject | null) => {
-      setObjectPath(!object ? null : object.path)
+      // If selecting object, navigate to details or commits and set clickedObject in location state
+      if (object) {
+        navigate(tabURL + viewSerializer({ ...params, objectPath: object.path }), { state: { clickedObject: object } })
+        return
+      }
+      // Otherwise, navigate back to view and remove objectPath query param
+      navigate(href("/view") + viewSerializer({ ...params, objectPath: null }))
     },
-    [setObjectPath]
+    [navigate, params, tabURL]
   )
 
   return { clickedObject, setClickedObject }
