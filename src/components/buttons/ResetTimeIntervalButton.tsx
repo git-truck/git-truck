8c583937dd87aeafb178b7ffe7d491de207ff6ec
nimrossum <1959615+nimrossum@users.noreply.github.com>
2026-03-02 10:19:15 +0100

    Add ResetTimeIntervalButton component

---
+++ src/components/buttons/ResetTimeIntervalButton.tsx
@@ -0,0 +1,33 @@
+import { mdiRestore } from "@mdi/js"
+import { Form } from "react-router"
+import { cn } from "~/styling"
+import { Icon } from "../Icon"
+import { useData } from "~/contexts/DataContext"
+import { useViewAction } from "~/hooks"
+
+export function ResetTimeIntervalButton() {
+  const data = useData()
+  const viewAction = useViewAction()
+
+  return (
+    <Form
+      className={cn({
+        invisible:
+          data.databaseInfo.timerange[0] === data.databaseInfo.selectedRange[0] &&
+          data.databaseInfo.timerange[1] === data.databaseInfo.selectedRange[1]
+      })}
+      method="post"
+      action={viewAction}
+    >
+      <input
+        type="hidden"
+        name="timeseries"
+        value={`${data.databaseInfo.timerange[0]}-${data.databaseInfo.timerange[1]}`}
+      />
+      <button className={cn("btn btn--text", {})}>
+        <Icon path={mdiRestore} />
+        Reset time interval
+      </button>
+    </Form>
+  )
+}
