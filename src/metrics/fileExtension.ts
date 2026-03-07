77da8a879cb4a4959cf20fde305c3323cb36a53a
Gustav Müller <guch@itu.dk>
2026-03-07 12:45:43 +0100

    Fix PointLegend functions as parent and scales with nested categories. Added feature flag for grouping other categories

--- src/metrics/fileExtension.ts
+++ src/metrics/fileExtension.ts
@@ -4,10 +4,10 @@ import type { MetricCache } from "~/metrics/metrics"
 import type { PointLegendData } from "~/components/legend/PointLegend"
 import { PointInfo } from "~/components/legend/PointLegend"
 import { noEntryColor } from "~/const"
+import { feature_flags } from "~/feature_flags"
 
 export function setExtensionColor(blob: GitBlobObject, cache: MetricCache) {
-  const extension = blob.name.substring(blob.name.lastIndexOf(".") + 1)
-  const extensionInfo = getColorFromExtension(extension)
+  const extensionInfo = getColorFromExtension(blob.extension)
   let color = extensionInfo?.color
 
   if (extensionInfo?.lang === "JSON") {
@@ -15,15 +15,37 @@ export function setExtensionColor(blob: GitBlobObject, cache: MetricCache) {
   }
 
   const legend = cache.legend as PointLegendData
-  if (color) {
-    if (legend.has(extension)) {
-      legend.get(extension)?.add(1)
+
+  //TODO: Make feature_flag a setting (grouping all non-colored entries under "Other" category)
+  //Add uncolored (e.g. png, gif) under parent category "Other"
+  if (feature_flags.group_noncode_filetypes_in_other) {
+    if (color) {
+      if (legend.has(blob.extension)) {
+        legend.get(blob.extension)?.add(1)
+      } else {
+        legend.set(blob.extension, new PointInfo(color, 1))
+      }
+      cache.colormap.set(blob.path, color)
     } else {
-      legend.set(extension, new PointInfo(color, 1))
+      if (!legend.has("Other")) {
+        legend.set("Other", new PointInfo(noEntryColor, 0))
+      }
+      if (legend.get("Other")?.children?.has(blob.extension)) {
+        legend.get("Other")?.children?.get(blob.extension)?.add(1)
+      } else {
+        console.warn(`Created extension ${blob.extension}, categorizing under "Other"`)
+        legend.get("Other")?.addChild(blob.extension, new PointInfo(noEntryColor, 1))
+      }
+      legend.get("Other")?.add(1)
+      cache.colormap.set(blob.path, noEntryColor)
     }
-    cache.colormap.set(blob.path, color)
+    //Add uncolored (e.g. png, gif) as normal categories
   } else {
-    if (!legend.has("Other")) legend.set("Other", new PointInfo(noEntryColor, 0))
-    cache.colormap.set(blob.path, noEntryColor)
+    if (legend.has(blob.extension)) {
+      legend.get(blob.extension)?.add(1)
+    } else {
+      legend.set(blob.extension, new PointInfo(color ? color : noEntryColor, 1))
+    }
+    cache.colormap.set(blob.path, color ? color : noEntryColor)
   }
 }
