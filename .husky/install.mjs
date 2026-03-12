748b6accdedd2857ce81a4b057a9d5cc2e8ac511
Jonas Røssum <1959615+nimrossum@users.noreply.github.com>
2026-03-12 09:59:55 +0100

    Migrate lint-staged to workflow instead
    Disable lint-staged and add on commit workflow that auto formats / auto fix and commit
    
    Fixes #878

--- .husky/install.mjs
+++
@@ -1,8 +0,0 @@
-import installHusky from "husky"
-
-// Skip Husky install in production and CI
-if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
-  process.exit(0)
-}
-
-console.log(installHusky())
