caaa2593f9ec11d62a0b4127653d4ca4220d6817
Jonas Nim Røssum <1959615+nimrossum@users.noreply.github.com>
2026-03-31 13:04:38 +0200

    feat: implement pickKey function with shared key type inference and add tests

--- src/shared/utils/object.ts
+++ src/shared/utils/object.ts
@@ -4,8 +4,19 @@ export const map = <O extends Record<string, unknown>, R = unknown>(
 ): { [K in keyof O]: R } =>
   Object.fromEntries(Object.entries(o).map(([k, v]) => [k, fn(v as O[keyof O], k as keyof O)])) as { [K in keyof O]: R }
 
-export const pickKey = <V extends Record<PropertyKey, unknown>, O extends Record<string, V>, K extends keyof V>(
-  o: O,
-  property: K
-): { [P in keyof O]: O[P][K] } =>
-  Object.fromEntries(Object.entries(o).map(([k, v]) => [k, (v as V)[property]])) as { [P in keyof O]: O[P][K] }
+type SharedKeys<T> = keyof {
+  [K in keyof T[keyof T] as T[keyof T] extends Record<K, unknown> ? K : never]: true
+}
+
+export function pickKey<T extends Record<PropertyKey, Record<PropertyKey, unknown>>, K extends SharedKeys<T>>(
+  obj: T,
+  key: K
+): { [P in keyof T]: T[P][K] } {
+  const result: Partial<{ [P in keyof T]: T[P][K] }> = {}
+
+  for (const k in obj) {
+    result[k] = obj[k][key]
+  }
+
+  return result as { [P in keyof T]: T[P][K] }
+}
