aacdf0cdc9ec1d46c06d17941485d4a04d974f24
Jonas Nim Røssum <1959615+nimrossum@users.noreply.github.com>
2026-05-06 21:31:46 +0200

    feat: incorporate new truck gifs

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,15 +1,16 @@
-import truck from "~/assets/truck.png"
+import parkedTruck from "~/assets/parkedTruck_48x.png"
+import crashedTruck from "~/assets/crashedTruck_48x.gif"
 import { cn } from "~/styling"
 
 export function ErrorPage({
   className,
   message,
-  noTruck = false,
+  truck = "crashed",
   children
 }: {
   className?: string
   message: string
-  noTruck?: boolean
+  truck?: "parked" | "crashed"
   children?: React.ReactNode
 }) {
   return (
@@ -17,9 +18,15 @@ export function ErrorPage({
       <div />
       <div className="grid h-full w-full place-items-center px-4 py-10">
         <div className="flex max-w-2xl flex-col items-center gap-3 text-center">
-          {!noTruck ? <img src={truck} alt="Git Truck" className="w-full max-w-sm min-w-0" /> : null}
           <div className="text-4xl leading-tight font-extrabold">{message}</div>
           {children}
+          {truck === "crashed" ? (
+            <img
+              src={truck === "crashed" ? crashedTruck : parkedTruck}
+              alt="Git Truck"
+              className="pixelated w-full min-w-0"
+            />
+          ) : null}
         </div>
       </div>
     </div>
