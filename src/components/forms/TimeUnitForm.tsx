3790d221e00b5134e74702b92acdc4e6bc5a2183
Jonas Nim Røssum <1959615+nimrossum@users.noreply.github.com>
2026-05-16 14:32:43 +0200

    refactor: clean up TimeUnitForm

--- src/components/forms/TimeUnitForm.tsx
+++ src/components/forms/TimeUnitForm.tsx
@@ -2,15 +2,12 @@ import { useQueryState } from "nuqs"
 import { useData } from "~/contexts/DataContext"
 import { viewSearchParamsConfig } from "~/routes/viewParams"
 import { TimeUnitValues, type TimeUnit } from "~/shared/utils/time"
-import { cn } from "~/styling"
 
 export function TimeUnitForm() {
   const data = useData()
   const [timeUnit, setTimeUnit] = useQueryState(
     "timeUnit",
-    viewSearchParamsConfig.timeUnit
-      .withOptions({ shallow: false })
-      .withDefault(data.databaseInfo.commitCountPerTimeIntervalUnit)
+    viewSearchParamsConfig.timeUnit.withDefault(data.databaseInfo.commitCountPerTimeIntervalUnit)
   )
 
   return (
@@ -21,31 +18,10 @@ export function TimeUnitForm() {
       onChange={(e) => setTimeUnit(e.target.value as TimeUnit)}
     >
       {TimeUnitValues.map((unit) => (
-        <option
-          key={unit}
-          className={cn(
-            "text-primary-text bg-primary-bg dark:text-primary-text-dark dark:bg-primary-bg-dark btn capitalize",
-            timeUnit === unit ? "btn--outlined" : "btn--text"
-          )}
-          value={unit}
-          onClick={() => setTimeUnit(unit)}
-        >
+        <option key={unit} value={unit}>
           {unit}
         </option>
       ))}
     </select>
-    // return (
-    //   <div className="flex gap-1">
-    //     {TimeUnitValues.map((unit) => (
-    //       <button
-    //         key={unit}
-    //         className={cn("btn capitalize", timeUnit === unit ? "btn--outlined" : "btn--text")}
-    //         value={unit}
-    //         onClick={() => setTimeUnit(unit)}
-    //       >
-    //         {unit}
-    //       </button>
-    //     ))}
-    //   </div>
   )
 }
