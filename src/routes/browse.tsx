a411857457b78834d1f0a161853a678b39998853
Jonas Nim Røssum <1959615+nimrossum@users.noreply.github.com>
2026-05-17 10:54:06 +0200

    fix browse layout and bg

--- src/routes/browse.tsx
+++ src/routes/browse.tsx
@@ -15,7 +15,8 @@ import {
   mdiSourceRepository,
   mdiFolder,
   mdiCheckboxBlank,
-  mdiCheckboxMarked
+  mdiCheckboxMarked,
+  mdiDotsVertical
 } from "@mdi/js"
 import MetadataDB from "~/server/MetadataDB"
 import { existsSync } from "node:fs"
@@ -267,10 +268,11 @@ export default function Index({
   }
 
   return (
-    <div className="mx-auto grid max-w-(--breakpoint-2xl) gap-2 p-2 lg:grid-cols-[var(--spacing-sidepanel)_1fr_var(--spacing-sidepanel)]">
+    <div className="mx-auto grid  max-w-(--breakpoint-2xl) gap-2 p-2 lg:grid-cols-[var(--spacing-sidepanel)_1fr_var(--spacing-sidepanel)]">
       <aside className="flex flex-col">
-        <div className="card sticky top-2">
+        <div className="card sticky top-2 flex flex-row justify-between items-center">
           <GitTruckInfo installedVersion={versionInfo.installedVersion} latestVersion={versionInfo.latestVersion} />
+          <Icon path={mdiDotsVertical} className="invisible" />
         </div>
       </aside>
       <main className="card">
