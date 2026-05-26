8ddc57cceb5e28add5b639321f91acfe75569fc9
Jonas Nim Røssum <1959615+nimrossum@users.noreply.github.com>
2026-05-26 20:45:17 +0200

    build(dev-deps): remove unnecessary dotenv dependency

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,4 @@
 import { defineConfig, devices } from "@playwright/test"
-import dotenv from "dotenv"
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-
-dotenv.config()
 
 /**
  * See https://playwright.dev/docs/test-configuration.
